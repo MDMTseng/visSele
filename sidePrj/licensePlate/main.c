888b2d68dd88bc3628163033d56b9744bd626640
Chia-Pin Tseng <chiapintseng@gmail.com>
2017-09-30 10:58:43 -0700

    update licensePlate prj

--- sidePrj/licensePlate/main.c
+++ sidePrj/licensePlate/main.c
@@ -27,10 +27,93 @@ void printImgAscii(acvImage *img, int printwidth)
         putchar('\n');
     }
 }
+void ImageSquare16(acvImage *res, acvImage *src)
+{
 
-//IIRCoeff
+    int width = src->GetWidth();
+    int height = src->GetHeight();
+
+    BYTE *srcfront;
+    BYTE *resfront;
+    for (int i = 0; i < height; i++)
+    {
+        srcfront = &(src->CVector[i][0]);
+        resfront = &(res->CVector[i][0]);
+        for (int k = 0; k < width; k++, resfront += 3, srcfront += 3)
+        {
+            int I=*srcfront;
+            I=I*I;
+            uint16_t *res16=(uint16_t*)(resfront+1);
+            *resfront=I>>8;
+            *res16=I;
+        }
+    }
+}
+
+void ImageDIff16_half(acvImage *reshalf_16, acvImage *img1_u16,acvImage *img2_u16)
+{
+
+    int width = reshalf_16->GetWidth();
+    int height = reshalf_16->GetHeight();
+
+    for (int i = 0; i < height; i++)
+    {
+
+        BYTE *img1front = &(img1_u16->CVector[i][0]);
+        BYTE *img2front = &(img2_u16->CVector[i][0]);
+        BYTE *reshalffront = &(reshalf_16->CVector[i][0]);
+
+        for (int k = 0; k < width; k++, img1front += 3, img2front += 3, reshalffront+=3)
+        {
+          uint16_t *img1_u16front = (uint16_t *)(img1front+1);
+          uint16_t *img2_u16front = (uint16_t *)(img2front+1);
+          int16_t *reshalf_16front = (int16_t *)(reshalffront+1);
+          int diff = (int)*img1_u16front-*img2_u16front;
+          *reshalffront = diff>>(1+8);//Including sign
+          *reshalf_16front=diff/2;
+        }
+    }
+}
+
+void ImageRm16(acvImage *res_16)
+{
+
+    int width = res_16->GetWidth();
+    int height = res_16->GetHeight();
+
+    for (int i = 0; i < height; i++)
+    {
+
+        BYTE *res16front = &(res_16->CVector[i][0]);
+
+        for (int k = 0; k < width; k++, res16front += 3)
+        {
+          uint16_t *res16 = (uint16_t *)(res16front+1);
+          *res16=0;
+        }
+    }
+}
 
 
+void GuidedFilter(acvImage *res, acvImage *Img, int r,
+  acvImage *buff1, acvImage *buff2, acvImage *buff3, acvImage *buff4)
+{
+    acvImage *mean=buff1;
+    acvCloneImage(mean, Img, 0);
+    //, acvImage *buff2, acvImage *buff3
+    acvBoxFilterX(buff2, mean, r);
+
+    acvImage *mean16_sq=buff2;
+    ImageSquare16(mean16_sq, mean);
+
+    acvImage *corr16=buff3;
+    ImageSquare16(corr16, Img);
+
+    //acvImage *var16_half=buff4;
+    //ImageDIff16_half(var16_half,corr16,Img16_sq);
+
+    //acvImage *a=1;
+}
 
 void XDiffDetect(acvImage *res, acvImage *src, int Size,int *LineBuff,int LineBuffL)
 {
@@ -90,6 +173,38 @@ void acvTurnX(acvImage *Pic)
     }
 }
 
+void acvBiliteralX(acvImage *res, acvImage *Img,int r,int tolerate)
+{
+    int i, j,k,m;
+    BYTE *resFront;
+    BYTE *imgLEdge;
+    BYTE *imgFront;
+    for (i = r; i < Img->GetHeight()-r; i++)
+    {
+        resFront = &(res->CVector[i][r*3]);
+        imgFront = &(Img->CVector[i][r*3]);
+        imgLEdge = &(Img->CVector[i-r][0]);
+        for (j = r; j < Img->GetWidth()-r; j++, resFront += 3, imgFront+=3, imgLEdge+=3)
+        {
+          int S=0;
+          int C=0;
+          for(k=0;k<(2*r+1);k++)for(m=0;m<(2*r+1);m++)
+          {
+            int swin=imgLEdge[(k*Img->GetWidth()+m)*3];
+            int diff=swin-(int)*imgFront;
+            if(diff<0)diff=-diff;
+            if(diff <tolerate)
+            {
+              S+=swin;
+              C+=1;
+            }
+          }
+          *resFront=S/C;
+
+        }
+    }
+}
+
 int testX()
 {
   acvImage *target = new acvImage();
@@ -98,24 +213,26 @@ int testX()
   acvImage *target_buff = new acvImage();
 
   target_buff->ReSize(target->GetWidth(), target->GetHeight());
-
+  acvClear(target_buff,0);
 
   clock_t t = clock();
+  acvBiliteralX(target_buff,target,3,40);
+  acvCloneImage(target_buff,target,0);
 
 
+  if(0)
+  {
 
-  int BUFFX[2000];
-  acvBoxFilterY_BL(target_buff, target, 1,BUFFX,sizeof(BUFFX)/sizeof(*BUFFX));
-  XDiffDetect(target, target_buff, 3,BUFFX,sizeof(BUFFX)/sizeof(*BUFFX));
+      int BUFFX[2000];
+      acvBoxFilterY_BL(target_buff, target, 1,BUFFX,sizeof(BUFFX)/sizeof(*BUFFX));
+      XDiffDetect(target, target_buff, 3,BUFFX,sizeof(BUFFX)/sizeof(*BUFFX));
+
 
   acvContrast(target,target,-20,4,0);
   acvBoxFilterX(target_buff, target, 1);
   acvBoxFilterX(target, target_buff, 2);
   acvTurnX(target);
   acvThreshold(target,250,0);
-
-  if(1)
-  {
 
   acvDrawBlock(target, 1, 1, target->GetWidth() - 2, target->GetHeight() - 2);
   acvComponentLabeling(target);
