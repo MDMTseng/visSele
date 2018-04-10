83069b4f24dbe76e0b227ace28b64a2006738d67
MDM Tseng <chiapintseng@gmail.com>
2018-04-09 23:28:36 -0700

    ss

--- Makefile_cc.sh
+++ Makefile_cc.sh
@@ -1,8 +1,7 @@
 
 TOOLCHAIN_DIR=/c/SysGCC/raspberry/
-export SYSROOT_FLAG="--sysroot=${TOOLCHAIN_DIR}/arm-linux-gnueabihf/sysroot"
 
-#TOOLCHAIN_PREFIX=${TOOLCHAIN_DIR}/bin/arm-linux-gnueabihf-
+TOOLCHAIN_PREFIX=${TOOLCHAIN_DIR}/bin/arm-linux-gnueabihf-
 export CC="${TOOLCHAIN_PREFIX}gcc.exe ${SYSROOT_FLAG}"
 export CXX="${TOOLCHAIN_PREFIX}g++.exe ${SYSROOT_FLAG}"
 export AR=${TOOLCHAIN_PREFIX}ar.exe
