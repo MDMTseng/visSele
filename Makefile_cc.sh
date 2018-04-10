b07ca0eef18c412ecdf7a63e6be819081146ff69
MDM Tseng <chiapintseng@gmail.com>
2018-04-09 21:24:20 -0700

    Add zlib
    Update compile script and example for zlib on Linux
    
    Update ignore

--- Makefile_cc.sh
+++ Makefile_cc.sh
@@ -1,9 +1,10 @@
 
 TOOLCHAIN_DIR=/c/SysGCC/raspberry/
-SYSROOT_FLAG=
-TOOLCHAIN_PREFIX=${TOOLCHAIN_DIR}/bin/arm-linux-gnueabihf-
-export CC=${TOOLCHAIN_PREFIX}gcc.exe $SYSROOT_FLAG
-export CXX=${TOOLCHAIN_PREFIX}g++.exe $SYSROOT_FLAG
-export AR=${TOOLCHAIN_PREFIX}ar.exe $SYSROOT_FLAG
 
+TOOLCHAIN_PREFIX=${TOOLCHAIN_DIR}/bin/arm-linux-gnueabihf-
+export CC="${TOOLCHAIN_PREFIX}gcc.exe ${SYSROOT_FLAG}"
+export CXX="${TOOLCHAIN_PREFIX}g++.exe ${SYSROOT_FLAG}"
+export AR=${TOOLCHAIN_PREFIX}ar.exe
+export RANLIB=${TOOLCHAIN_PREFIX}ranlib.exe
+export PREFIX=${TOOLCHAIN_PREFIX}
 make
