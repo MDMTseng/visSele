4e0c4d87b6bed3f5d9bfb7ddf4338974c63833c7
Chia-Pin Tseng <chiapintseng@gmail.com>
2018-04-28 15:01:48 -0700

    tmp

---
+++ MatchingEngine/MorphEngine/include/MorphEngine.h
@@ -0,0 +1,18 @@
+#ifndef MORPHENGINE_HPP
+#define MORPHENGINE_HPP
+
+#include "acvImage_ToolBox.hpp"
+
+#include "acvImage_BasicDrawTool.hpp"
+#include <cstdlib>
+#include <unistd.h>
+
+
+class MorphEngine{
+
+    public:
+    MorphEngine(int gridNumber);
+};
+
+
+#endif
