4559073c6827a750dc1de06e03a0c0fa7b1d4688
MDM Tseng <chiapintseng@gmail.com>
2018-11-14 22:48:20 +0800

    Add byteArrString to print HEX arr

--- logctrl/include/logctrl.h
+++ logctrl/include/logctrl.h
@@ -1,6 +1,7 @@
 #ifndef LOGCTRL_HEADER
 #define LOGCTRL_HEADER
 #include <stdio.h>
+#include <stdint.h>
 
 
 #define VA_ARGS(...) , ##__VA_ARGS__
@@ -10,10 +11,11 @@ void logv(const char *fmt, ...);
 void logd(const char *fmt, ...);
 void logi(const char *fmt, ...);
 void loge(const char *fmt, ...);
+char* byteArrString(uint8_t *data, int length,int spaceInterval);
+char* byteArrString(uint8_t *data, int length);
 
 #define LOGV(fmt,...) logv("%s:v " fmt "\n",__func__ VA_ARGS(__VA_ARGS__))
 #define LOGD(fmt,...) logd("%s:d " fmt "\n",__func__ VA_ARGS(__VA_ARGS__))
 #define LOGI(fmt,...) logi("%s:i " fmt "\n",__func__ VA_ARGS(__VA_ARGS__))
 #define LOGE(fmt,...) loge("%s:e " fmt "\n",__func__ VA_ARGS(__VA_ARGS__))
-
 #endif
--- logctrl/logctrl.c
+++ logctrl/logctrl.c
@@ -49,3 +49,41 @@ void loge(const char *fmt, ...)
     log_error(fmt, ap);
     va_end(ap);
 }
+
+
+
+char* byteArrString(uint8_t *data, int length)
+{
+    return byteArrString(data,  length,4);
+}
+
+char* byteArrString(uint8_t *data, int length,int spaceInterval)
+{
+    const int buffL = 500;
+    static char buff[buffL+30];
+
+    int maxArrL = buffL/(spaceInterval*2+1)*spaceInterval;//spaceInterval=4 => print is 095A4B65 226C7CC8 AABCDBF4
+    int pLength = length;
+    if(pLength>maxArrL)
+    {
+        pLength = maxArrL;
+    }
+    char *strptr=buff;
+
+    int scount=0;
+    for (int i = 0; i < pLength; i++)
+    {
+        strptr+=sprintf(strptr,"%02X", data[i]);
+        if(scount++==spaceInterval)
+        {
+            strptr+=sprintf(strptr," ");
+            scount=0;
+        }
+    }
+    if(pLength!=length)
+    {
+        strptr += sprintf(strptr,"...(%d more)", length - pLength);
+    }
+
+    return buff;
+}
