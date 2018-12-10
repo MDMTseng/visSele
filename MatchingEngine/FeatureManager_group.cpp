a367d97a1965cde0e10661c0fcad7bfef8532e73
MDM Tseng <chiapintseng@gmail.com>
2018-12-10 16:07:51 +0800

    XX

--- MatchingEngine/FeatureManager_group.cpp
+++ MatchingEngine/FeatureManager_group.cpp
@@ -135,15 +135,24 @@ bool FeatureManager_binary_processing_group::check(cJSON *root)
     return false;
 }
 
+
 int FeatureManager_binary_processing_group::FeatureMatching(acvImage *img,acvImage *buff,acvImage *dbg)
 {
     ldData.resize(0);
-    acvThreshold(img, 128, 0);
-    acvDrawBlock(img, 1, 1, img->GetWidth() - 2, img->GetHeight() - 2);
+    binary_img.ReSize(img->GetWidth(),img->GetHeight());
 
-    acvComponentLabeling(img);
-    acvLabeledRegionInfo(img, &ldData);
-    if(ldData.size()-1<1)
+    acvThreshold(&binary_img, 128, 0);
+
+    //Draw a labeling black cage for labling algo, which is needed for acvComponentLabeling
+    acvDrawBlock(&binary_img, 1, 1, binary_img.GetWidth() - 2, binary_img.GetHeight() - 2);
+
+
+    //The labeling starts from (1 1) => (W-2,H-2), ie. it will not touch the outmost pixel to simplify the boundary condition
+    //You need to draw a black/white cage to work(not crash).
+    //The advantage of black cage is you can know which area touches the boundary then we can exclude it
+    acvComponentLabeling(&binary_img);
+    acvLabeledRegionInfo(&binary_img, &ldData);
+    if(ldData.size()<=1)
     {
       return 0;
     }
@@ -157,7 +166,8 @@ int FeatureManager_binary_processing_group::FeatureMatching(acvImage *img,acvIma
 
     for(int i=0;i<binaryFeatureBundle.size();i++)
     {
-      binaryFeatureBundle[i]->FeatureMatching(img,buff,ldData,dbg);
+      binaryFeatureBundle[i]->setOriginalImage(img);
+      binaryFeatureBundle[i]->FeatureMatching(&binary_img,buff,ldData,dbg);
     }
   return 0;
 }
