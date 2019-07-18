8da2153fa37405858d400dce6f6b180b71013e84
MDM Tseng <chiapintseng@gmail.com>
2019-07-19 03:03:22 +0800

    no message

---
+++ InspectionCore/sidePrj/opencv_test/opencv_test.c
@@ -0,0 +1,15 @@
+
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include <math.h>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv)
+{
+
+    Mat img = Mat::zeros(20, 20, CV_8UC1);
+
+    return 0;
+}
