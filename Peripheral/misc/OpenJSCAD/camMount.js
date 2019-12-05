09886b08ebbf81486395131d2125eeb71936bcd6
Chia-Pin Tseng <chiapintseng@gmail.com>
2019-12-06 02:04:24 +0800

    OpenJSCad add Pentagon gopro mount for plating check prototype

--- Peripheral/misc/OpenJSCAD/camMount.js
+++ Peripheral/misc/OpenJSCAD/camMount.js
@@ -4,37 +4,109 @@
 // revision   : 0.003
 // tags       : Logo,Intersection,Sphere,Cube
 // file       : logo.jscad
-const {RotateJoint_parts} = require('./joint.js')
+const {RotateJoint_parts,GoProJoint_parts,PolyRod} = require('./joint.js')
+//const stlDeSerializer = require('@jscad/stl-deserializer')
 
-
+let fn=10;
 function camModule(){
   
-  let lens=cylinder({size:1, center: true}).scale([.3,0.3,0.5]).rotateY(90);
+  let lens=cylinder({size:1, center: true,fn}).scale([.3,0.3,0.5]).rotateY(90);
   let camBody=color("Red",cube({size:1, center: true}).scale([0.1,1,1]));
   let camHolder=cube({size:1, center: true});
-  
+  let camera = union( 
+    color("blue",lens.translate([-0.3, 0,0]))
+    ,color("Red",camBody)
+    ,camHolder.translate([(0.1+1)/2, 0,0]))
   return union(
-      color("blue",
-          lens.translate([-0.3, 0,0])
-      ),
-      color("Red",camBody),
-      
-      camHolder.translate([(0.1+1)/2, 0,0]),
-      RotateJoint_parts().map(part=>part.scale(0.3).translate([0.5, 0,1]))
-      );
+    camera.scale(10)
+    ,GoProJoint_parts().map(part=>part.scale(0.3).translate([5, 0,10]))
+    );
+}
+
+
+function GoProMount_Screw(mountLen=12,stickLen=18,stickThickness=7,stickWedgedRate=0.9)
+{
+  console.log(">>GoProMount_Screw call GoProJoint_parts");
+  let GPJoint=GoProJoint_parts(mountLen);
+  let GPJ2=(GPJoint[1]).rotateZ(180);
+  let endPlate=cube({center:true}).scale([5,15,16]).translate([-mountLen-2,0,0.5]);
+  let rod_len=stickLen;
+  let rod=cylinder({r1: 1,r2:stickWedgedRate,center:true,fn:4})
+  .rotateZ(-45).rotateY(-90).scale([1,1/Math.SQRT2,1/Math.SQRT2]).scale([rod_len,stickThickness,stickThickness]).translate([-mountLen-rod_len/2,0,0]);
+  return union(GPJ2,endPlate,rod)
+}
+
+
+
+function CamHolder(legnth=10)
+{
+  let GPJoint=GoProJoint_parts(legnth);
+
+  //let GPJ2=(GPJoint[1]).rotateZ(180);
+
+  return union(
+    union(GPJoint[0]).rotateX(90).rotateY(90).translate([0,0,legnth]),
+    sheel1()
+    )
 }
 
 
 function multiCamHolder()
 {
-  let cholder=camModule().rotateY(-45).translate([5, 0,0]);
+  let cholder = CamHolder().rotateY(-45).translate([50, 0,0])
   let hArr=[]
+
+  //PolyRod(5,-Math.PI*2/5/4).scale([30,30,30])
   for(let i=0;i<5;i++)
   {
-      hArr.push(cholder.rotateZ(360/5*i));
+      hArr.push((cholder).rotateZ(360/5*i)); 
   }
-  return hArr
+  return (hArr)
   
+} 
+
+ 
+function HolderMountStick()
+{ 
+  let screw=GoProMount_Screw(mountLen=12,stickLen=18,stickThickness=7,stickWedgedRate=0.9).translate([35,0,0]).rotateX(90).rotateY(0);
+   
+  // screw.polygons.forEach((vex)=>{
+  //   vex.vertices.forEach((v)=>{
+  //     if(v.pos.dd===undefined)
+  //     {
+  //       v.pos.x*=0.9;
+  //       v.pos.y*=0.9;
+  //       v.pos.z*=0.9;
+  //       v.pos._x*=0.9;
+  //       v.pos._y*=0.9;
+  //       v.pos._z*=0.9;
+  //     }
+  //     v.pos.dd=5;
+  //   })
+  // });
+  //console.log(screw.polygons)
+  return screw;
+} 
+
+function HolderBase()
+{
+  let screw=HolderMountStick();
+ 
+  let screwBase=GoProJoint_parts(20)[0].rotateY(90).translate([0,0,20]);
+  screwBase=union(
+    screwBase.translate([0,0,0])
+  ).rotateZ(360/5/4);
+
+
+  let mountBase = difference(
+    union(
+      PolyRod(5,-360/5/2).scale([25,25,20]),
+      screwBase), 
+    union([0,1,2,3,4].map(idx=>screw.rotateZ(360*idx/5))))
+
+  return union(
+    mountBase.rotateZ(-360/5/4)
+    )
 } 
 
 function sheel1(){
@@ -46,25 +118,56 @@ function sheel1(){
   // let faceRight=cube({size:1, center: true}).scale([50,44,1]);
   // let faceBack=cube({size:1, center: true}).scale([1,13,1]);
   let drillHoleOffset=((40-36)/2);
+  let hole=cylinder({r: 3/2, h: 100,fn});
   let holes=union(
-      cylinder({r: 3/2, h: 100}).translate([drillHoleOffset, 4.5, 0])
-      ,cylinder({r:3/2, h: 100}).translate([drillHoleOffset,20+4.5, 0])
+    hole.translate([drillHoleOffset, 4.5, 0])
+      ,hole.translate([drillHoleOffset,20+4.5, 0])
       
-      ,cylinder({r:3/2, h: 100}).translate([40-2,14.5, 0])
+      ,hole.translate([40-2,14.5, 0])
       
-      ,cylinder({r: 3/2, h: 100}).translate([40-drillHoleOffset,(29-22)/2, 0])
-      ,cylinder({r: 3/2, h: 100}).translate([40-drillHoleOffset,29-(29-22)/2, 0])
+      ,hole.translate([40-drillHoleOffset,(29-22)/2, 0])
+      ,hole.translate([40-drillHoleOffset,29-(29-22)/2, 0])
       );
   return difference(
       plate.rotateZ(0),
       holes
-  ).translate([0,0,0])
+  ).translate([-40/2,-29/2,-3/2])
+}
+function sShell(){
+  let plate=union(
+      // cylinder({r: 1.5, h: 1}).translate([0, 0, 0]),
+      cube({size:1, center: false}).scale([20,29,3])
+      // .setColor([12,12,0])
+      )
+  // let faceRight=cube({size:1, center: true}).scale([50,44,1]);
+  // let faceBack=cube({size:1, center: true}).scale([1,13,1]);
+  let drillHoleOffset=((40-36)/2);
+  let hole=cylinder({r: 3/2, h: 100,fn});
+  let holes=union(
+    hole.translate([drillHoleOffset, 4.5, 0])
+      ,hole.translate([drillHoleOffset,20+4.5, 0])
+      
+      );
+  return difference(
+      plate.rotateZ(0),
+      holes
+  ).translate([-20/2,-29/2,-3/2])
 }
 
-function main () {
+function main (p) {
   //let obj = union(sampleCube()).translate([5, 0,0])
-  return union(
-      [camModule(),sheel1()]
-      
-  );
+  // const rawData = fs.readFileSync('/Users/mdm/visSele/Peripheral/misc/OpenJSCAD/simpleCube.stl')
+  // const csgData = stlDeSerializer.deserialize(rawData, undefined, {output: 'csg'})
+
+  let showObj=union(
+    [
+      HolderBase(),
+      //GoProMount_Screw(),
+      //HolderMountStick(),
+      //multiCamHolder()
+      //cylinder({fn:5}),
+      new CSG()//Empty, just for 
+    ])
+  console.log(">>.Done");
+  return showObj;
 }
--- Peripheral/misc/OpenJSCAD/joint.js
+++ Peripheral/misc/OpenJSCAD/joint.js
@@ -1,8 +1,10 @@
-const {cylinder,cube} = require('@jscad/csg/api').primitives3d
-const {color} = require('@jscad/csg/api').color
-const {difference,union} = require('@jscad/csg/api').booleanOps
-const {translate} = require('@jscad/csg/api').transformations
-
+const CSG_api=require('@jscad/csg/api');
+  
+const {cylinder,cube} = CSG_api.primitives3d
+const {color} = CSG_api.color
+const {difference,union} =CSG_api.booleanOps
+const {translate} =CSG_api.transformations
+let fn=10;
 function ballJoint_parts(){
   let translateXYZ=[0,0,0]
   let ball=union(sphere({r: 1, fn: 36}))
@@ -19,7 +21,7 @@ function ballJoint_parts(){
           )
   
   let ballCoverRodD= 0.4;
-  let ballRod = cylinder({size:1, center: true}).
+  let ballRod = cylinder({size:1, center: true,fn}).
           translate([0,0,0.5]).scale([ballCoverRodD,ballCoverRodD,4]).rotateY(-90)
   
   
@@ -40,18 +42,18 @@ function ballJoint_parts(){
       ];
 }
 
-function RotateJoint_parts(){
 
 
-  let basePlate=cylinder({size: 1, center: true}).scale([1,1,1/3.01])
-  basePlate=union(basePlate)
-  let plate0= basePlate.translate([0,0,-2/3.0])
-  let plate4= basePlate.translate([0,0,2/3.0])
-  let plateXX=union(plate0,plate4,basePlate).rotateX(90)
+function RotateJoint_parts(interval=0.3,d=1.5,plateshrinkRatio=0.99){
+
+  let basePlate=cylinder({d: d, center: true,fn}).scale([1,1,interval*plateshrinkRatio])
+  let plate0= basePlate.translate([0,0,-2*interval])
+  let plate4= basePlate.translate([0,0,2*interval])
+  let plateXX=union([plate0,basePlate,plate4].map(p=>p.rotateX(90)))
   
-  let plate1= basePlate.translate([0,0,-1/3.0])
-  let plate3= basePlate.translate([0,0,1/3.0])
-  let plateTB=union(plate1,plate3).rotateX(90)
+  let plate1= basePlate.translate([0,0,-1*interval])
+  let plate3= basePlate.translate([0,0,1*interval])
+  let plateTB=union([plate1,plate3].map(p=>p.rotateX(90)))
 
   return [
       plateTB,color("red",plateXX)
@@ -59,14 +61,71 @@ function RotateJoint_parts(){
 }
 
 
+function HEX(radius=1,height=1){
+  return PolyRod(polyCount=6,angleOffset=0).scale([radius,radius,height]);
+}
+
+
+
+function PolyRod(polyCount=5,angleOffset=0){
+  return cylinder({center:true,fn:polyCount}).rotateZ(angleOffset).scale([1,1,1]);
+}
+
+function GoProScrew(length=5)
+{
+  let nutR=4.4*1.1;
+  let nutH=4.8;
+  let nutHole=HEX().scale([nutR,nutR,nutH]).translate([0,0,nutH/2]); 
+  let screwRod=cylinder({d: 5, center: true,fn}).scale([1,1,length]).translate([0,0,-length/2]);
+
+  return union(nutHole,screwRod)
+}
+function GoProJoint_plate(Leglenth=20)
+{
+  let thickness=2.93;
+  let cylD=15;
+  let fn=20
+  let plate=cylinder({d: cylD, center: true,fn}).scale([1,1,thickness]);
+  plate=union(plate,cube({size: [Leglenth,cylD,thickness], center:true}).translate([Leglenth/2,0,0])) 
+  let screwRod=cylinder({d: 5, center: true,fn}).scale([1,1,20]);
+
+
+  return difference(plate,screwRod)
+}
+
+
+function JointPlatOffset(plateModel,offsetXYZ=[1,0,0],idx=0,totC=3){
+  let offset=offsetXYZ.map(ofs=>ofs*(idx-(totC-1)/2)    );
+  return plateModel.translate(offset);
+}
+
+
+function GoProJoint_parts(legL=10){
+  
+  let gplat=GoProJoint_plate(legL);
+  let plats = [0,1,2,3,4].map(idx=>JointPlatOffset(gplat,offsetXYZ=[0,0,3],idx,5));
+  let objPlate=union([plats[1],plats[3]]);
+  
+  let lastPlate=union(plats[4],plats[4].translate([0,0,1]));
+  
+  lastPlate=difference(lastPlate, GoProScrew().translate([0,0,6.5]))
+  let podPlate=union([plats[0],plats[2],lastPlate]);
+  return [
+    objPlate,podPlate
+  ];
+}
+
+
+
+
 function joint_middle(angle)
 {
   return union([
-      cylinder({size: 1, center: true}).scale([1,1,1/3]).translate([0,0,-1/3.0]),
-      cylinder({size: 1, center: true}).scale([1,1,1/3]).rotateZ(angle),
-      cylinder({size: 1, center: true}).scale([1,1,1/3]).translate([0,0,1/3.0])
+      cylinder({size: 1, center: true,fn}).scale([1,1,1/3]).translate([0,0,-1/3.0]),
+      cylinder({size: 1, center: true,fn}).scale([1,1,1/3]).rotateZ(angle),
+      cylinder({size: 1, center: true,fn}).scale([1,1,1/3]).translate([0,0,1/3.0])
       ]);
 }
 
 
-module.exports={ballJoint_parts,RotateJoint_parts,joint_middle}
+module.exports={ballJoint_parts,RotateJoint_parts,GoProJoint_parts,joint_middle,PolyRod}
