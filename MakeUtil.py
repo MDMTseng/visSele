a8028ab2be8cd23ea050e26a228030f7a07b3e7c
Chia-Pin Tseng <chiapintseng@gmail.com>
2020-06-11 19:01:32 +0800

    project make: add a python script for final update file generation

---
+++ MakeUtil.py
@@ -0,0 +1,110 @@
+
+
+import getopt, sys
+import zipfile
+import shutil
+import urllib.request
+import websockets
+import asyncio
+import json
+import subprocess
+import ntpath
+import signal
+import os
+from time import sleep
+from datetime import datetime
+import platform
+import requests
+
+path_env=os.path.abspath("./")
+path_script=os.path.abspath(sys.argv[0])
+path_local=os.path.dirname(os.path.dirname(path_script))
+
+print("path_env=",path_env)
+print("path_script=",path_script)
+print("path_local=",path_local)
+
+
+BIN_DIR="Xception"
+
+
+
+WebUI_Path=path_env+"/WebUI"
+Core_Path=path_env+"/Core"
+
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+def parse_argv(argv):
+  try:
+    opts, args = getopt.getopt(argv[1:], "", 
+    [
+      'type=',
+      'src_dir=',
+      'dst_path='
+
+    ])
+  except getopt.GetoptError as err:
+    # print help information and exit:
+    print("ERROR",err) 
+    #print(err)  # will print something like "option -a not recognized"
+    #usage()
+    return None
+
+  print(opts, args) 
+  obj={}
+  for o, a in opts:
+    key=o[2:]
+    obj[key]=a
+
+  return obj
+
+
+def zip_add_dir(path, ziph):
+  # ziph is zipfile handle
+  for root, dirs, files in os.walk(path):
+    for file in files:
+      #print(root,file)
+      ziph.write(os.path.join(root, file))
+
+
+def cmd_exec(cmd):
+  infoObj={}
+  _type=cmd.get("type", None)
+  ACK=-10
+  if _type == "zip":
+    
+    if os.path.isfile(cmd["dst_path"]):
+      print("Err:dst_path:",cmd["dst_path"]," exists")
+    else:
+      # print("src_dir",cmd["src_dir"])
+      # print("dst_path",cmd["dst_path"])
+      zf = zipfile.ZipFile(cmd["dst_path"], mode='w')
+      zip_add_dir(cmd["src_dir"],zf )
+      zf.close()
+      ACK=0
+    pass
+      
+  elif _type == "un_deploy":#in case of deploy error, roll back
+    pass
+  elif _type == "EXIT":
+    ACK=0
+  
+  
+  return ACK,infoObj
+
+if __name__ == "__main__":
+
+  obj= parse_argv(sys.argv)
+  _type=obj.get("type", None)
+
+  if  _type is not None:
+    retx,info=cmd_exec(obj)
+    print(retx)
+    sys.exit(retx)
+  else:
+    sys.exit(-33)
+
+
--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-domake:build export
+domake:build export zip_update
 
-.PHONY:build export
+.PHONY:build export zip_update
 
 
 build: 
@@ -9,10 +9,19 @@ build:
 
 
 EXPFolder=export
-export: 
+export:
 	-@rm -r $(EXPFolder)
 	-@mkdir -p $(EXPFolder)/WebUI
 	cd UI/WebUI; sh export.sh ../../$(EXPFolder)/WebUI
 	-@mkdir -p $(EXPFolder)/Core
 	make -C $(abspath .)/InspectionCore export EXPORT_PATH=$(abspath .)/$(EXPFolder)/Core
 	cp -r scripts $(EXPFolder)
+
+
+	
+UPDATE_FILE_NAME=update
+zip_update:
+	-@rm -r $(UPDATE_FILE_NAME) $(UPDATE_FILE_NAME).zip
+	cp -r $(EXPFolder) $(UPDATE_FILE_NAME)
+	python MakeUtil.py --type=zip --src_dir=$(UPDATE_FILE_NAME) --dst_path=$(UPDATE_FILE_NAME).zip
+	-@rm -r $(UPDATE_FILE_NAME)
