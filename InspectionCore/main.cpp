da860750298f7e19c11e8c8765be2b24e8f61bf0
Chia-Pin Tseng <chiapintseng@gmail.com>
2020-10-21 20:55:07 +0800

    2/2 : Core: move main.cpp back

---
+++ InspectionCore/main.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <signal.h>
+
+//#include "MLNN.hpp"
+#include "cJSON.h"
+#include "logctrl.h"
+#include "DatCH_Image.hpp"
+#include "tmpCodes.hpp"
+#include "DatCH_WebSocket.hpp"
+#include "DatCH_BPG.hpp"
+#include "DatCH_CallBack_WSBPG.hpp"
+#include "MatchingCore.h"
+
+#include <compat_dirent.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <libgen.h>
+#include <main.h>
+#include <playground.h>
+#include <stdexcept>
+
+#include <RingBuf.hpp>
+
+#include <lodepng.h>
+
+
+
+
+
+
+#include <vector>
+int main(int argc, char **argv)
+{
+
+  return cp_main(argc, argv);
+}
+
