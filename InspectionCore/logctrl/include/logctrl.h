d70ac8f9a415b39a97402f54281188cca698055e
Chia-Pin Tseng <chiapintseng@gmail.com>
2021-03-13 14:29:04 +0800

    LOG update

--- InspectionCore/logctrl/include/logctrl.h
+++ InspectionCore/logctrl/include/logctrl.h
@@ -25,11 +25,15 @@ char* _SubString_Align(const char* str,int Count);
 #define LOG_FILENAME_L 10
 #define LOG_HAS_LINENUMBER 
 #ifdef LOG_HAS_LINENUMBER
+
+#define C_TIME (((double)clock()/ CLOCKS_PER_SEC * 1000))
+
 #define LOGV(fmt,...) //logv("%s%04d %s:v " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
 #define LOGD(fmt,...) //logd("%s%04d %s:d " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
 #define LOGI(fmt,...) logi("%s%04d %s:i " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
 #define LOGE(fmt,...) loge("%s%04d %s:e " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
-#define LOGS(buf,fmt,...) sprintf(buf,"%s%04d %s:$ " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
+// #define LOGE(fmt,...) loge("%f:%s%04d %s:e " fmt "\n",C_TIME,_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
+#define LOGS(buf,fmt,...) sprintf(buf,"%f:%s%04d %s:$ " fmt "\n",C_TIME,_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
 #else
 #define LOGV(fmt,...) logv("%s %s:v " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__func__ VA_ARGS(__VA_ARGS__))
 #define LOGD(fmt,...) logd("%s %s:d " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__func__ VA_ARGS(__VA_ARGS__))
