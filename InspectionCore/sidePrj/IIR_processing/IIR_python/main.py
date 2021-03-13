b9ff1215d668d6b96cb00faeeb2071af5c1c7e79
Chia-Pin Tseng <chiapintseng@gmail.com>
2021-03-13 14:24:11 +0800

    side proj

--- InspectionCore/sidePrj/IIR_processing/IIR_python/main.py
+++ InspectionCore/sidePrj/IIR_processing/IIR_python/main.py
@@ -5,6 +5,7 @@ from IIR_Filter import *
 
 
 
+
 def TriCos_window(n,N, a0, a1, a2, a3):
 
   n2PI_div_N=n*2*np.pi/N
@@ -41,63 +42,124 @@ def chaseFilter(x,stepSize=1):
 
   return y
    
-
-fs=60
-N = 32-1 #single side of filter 
-x = np.linspace(0,N-1, N)
-# y=1-(x+1)/40
-
-
-# y_window = TriCos_window(np.linspace(N,N*2-1, N),N*2, 0.35875, 0.48829, 0.14128, 0.01168)
-y_window = sigmoid(0.3*(-x+N/3))
-f_stop=2/(fs/2/N)
-y=y_window*sigmoid_well(x,f_stop,f_stop*1.2,2.5)* (1*(x<f_stop)+.2*(x>f_stop))
-# y=np.convolve(y,np.linspace(1/3,1/3, 3), 'same')
-DCGain=1
-HfreqGain=y[-1]
-
-#make the spectrum mirrored so that ifft would have real number only result
-Y=np.concatenate(([DCGain],y,[HfreqGain] ,y[::-1]))
-yf=scipy.fftpack.ifft(Y)
-
-fullN=yf.size
-
-#following are the singlesided filter
-BlackManHarris_window = TriCos_window(np.linspace(fullN//2,fullN-1, fullN//2),fullN, 0.35875, 0.48829, 0.14128, 0.01168)
-filter_fss=np.real(yf[:fullN//2])*BlackManHarris_window
+def test1():
+  fs=60
+  N = 32-1 #single side of filter 
+  x = np.linspace(0,N-1, N)
+  # y=1-(x+1)/40
 
 
-filter_full=np.concatenate((filter_fss[::-1],filter_fss[1:]))
-filter_full=filter_full/np.sum(filter_full)#make the DC gain 1
+  # y_window = TriCos_window(np.linspace(N,N*2-1, N),N*2, 0.35875, 0.48829, 0.14128, 0.01168)
+  y_window = sigmoid(0.3*(-x+N/3))
+  f_stop=2/(fs/2/N)
+  y=y_window*sigmoid_well(x,f_stop,f_stop*1.2,2.5)* (1*(x<f_stop)+.2*(x>f_stop))
+  # y=np.convolve(y,np.linspace(1/3,1/3, 3), 'same')
+  DCGain=1
+  HfreqGain=y[-1]
 
-t = np.linspace(0,200-1, 200)
+  #make the spectrum mirrored so that ifft would have real number only result
+  Y=np.concatenate(([DCGain],y,[HfreqGain] ,y[::-1]))
+  yf=scipy.fftpack.ifft(Y)
 
-testY = (sigmoid_square(t,fs,fs/1.5,0.3) + sigmoid_square(t,fs*2.5,fs,4)  )
-testChaseY=chaseFilter(testY,0.06)
-result = np.convolve(filter_full,testChaseY)
+  fullN=yf.size
 
-res_t = np.linspace(0,result.size-1, result.size)-filter_full.size/2
+  #following are the singlesided filter
+  BlackManHarris_window = TriCos_window(np.linspace(fullN//2,fullN-1, fullN//2),fullN, 0.35875, 0.48829, 0.14128, 0.01168)
+  filter_fss=np.real(yf[:fullN//2])*BlackManHarris_window
 
 
-plt.figure("FFT & FIR")
+  filter_full=np.concatenate((filter_fss[::-1],filter_fss[1:]))
+  filter_full=filter_full/np.sum(filter_full)#make the DC gain 1
 
-plt.subplot(211)
-freqAxis = np.linspace(0,filter_fss.size-1-1, filter_fss.size-1)*fs/2/(filter_fss.size-1)
-plt.plot(freqAxis,y, 'bo',freqAxis,y,  'r--')
-plt.subplot(212)
-plt.plot(filter_fss)
+  t = np.linspace(0,200-1, 200)
 
-plt.figure("Conv test")
-plt.plot(t,testY)
-plt.plot(t,testChaseY)
-plt.plot(res_t,result)
+  testY = (sigmoid_square(t,fs,fs/1.5,0.3) + sigmoid_square(t,fs*2.5,fs,4)  )
+  testChaseY=chaseFilter(testY,0.06)
+  result = np.convolve(filter_full,testChaseY)
 
-plt.show()
-# y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
-# yf = scipy.fftpack.fft(y)
-# print(np.real(yf[0]))
-# xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+  res_t = np.linspace(0,result.size-1, result.size)-filter_full.size/2
 
-# fig, ax = plt.subplots()
-# ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
-# plt.show()
+
+  plt.figure("FFT & FIR")
+
+  plt.subplot(211)
+  freqAxis = np.linspace(0,filter_fss.size-1-1, filter_fss.size-1)*fs/2/(filter_fss.size-1)
+  plt.plot(freqAxis,y, 'bo',freqAxis,y,  'r--')
+  plt.subplot(212)
+  plt.plot(filter_fss)
+
+  plt.figure("Conv test")
+  plt.plot(t,testY)
+  plt.plot(t,testChaseY)
+  plt.plot(res_t,result)
+
+  plt.show()
+  # y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
+  # yf = scipy.fftpack.fft(y)
+  # print(np.real(yf[0]))
+  # xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+
+  # fig, ax = plt.subplots()
+  # ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
+  # plt.show()
+
+
+
+class BoxFilter:
+  def __init__(self, size,defaltValue=0):
+    self.buffer=0
+    self.curSum=0
+    self.curIdx=0
+    self.set(size,defaltValue)
+  
+  def set(self,size,defaltValue=0):
+    self.buffer=np.linspace(defaltValue, defaltValue, size)
+    self.curSum=defaltValue*size
+    self.curIdx=0
+
+  def push(self,newValue):
+    self.curSum-=self.buffer[self.curIdx]
+    self.curSum+=newValue
+    self.buffer[self.curIdx]=newValue
+    self.curIdx=(self.curIdx+1)% len(self.buffer)
+
+  def h(self,newValue=None):
+    if newValue is not None:
+      self.push(newValue)
+    return self.curSum/len(self.buffer)
+    
+
+
+class BoxFilter_XYZ:
+  def __init__(self, size,defaltXYZ=[0,0,0]):
+    self.Filters=[]
+    self.set(size,defaltXYZ)
+  
+  def set(self,size,defaltXYZ=[0,0,0]):
+    self.Filters=[BoxFilter(size,defaltXYZ[0]),BoxFilter(size,defaltXYZ[1]),BoxFilter(size,defaltXYZ[2])]
+
+  def h(self,newValue=[None,None,None]):
+    # print(self)
+    return [self.Filters[0].h(newValue[0]),self.Filters[1].h(newValue[1]),self.Filters[2].h(newValue[2])]
+
+if __name__ == '__main__':
+  bF =BoxFilter(20)
+  print(bF.h(0))
+  print(bF.h(0))
+  print(bF.h(1))
+
+
+  
+  bF_XYZ =BoxFilter_XYZ(20)
+  print(bF_XYZ.h([1,1,1]))
+  print(bF_XYZ.h([10,10,10]))
+  print(bF_XYZ.h([10,10,10]))
+  print(bF_XYZ.h([10,10,10]))
+  print(bF_XYZ.h([1,1,1]))
+  bF_XYZ.set(20,[1,1,1])
+  print(bF_XYZ.h([50,50,50]))
+  print(bF_XYZ.h([1,1,1]))
+  print(bF_XYZ.h([50,50,50]))
+  print(bF_XYZ.h([1,1,1]))
+  print(bF_XYZ.h([1,1,1]))
+  print(bF_XYZ.h([1,1,1]))
