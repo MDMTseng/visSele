3deafe10a70f8d1a937bf0505a5fb038988d7ed9
Chia-Pin Tseng <chiapintseng@gmail.com>
2021-04-12 15:20:09 +0800

    MJPEG writer update

--- InspectionCore/contrib/MJPEGWriter/main.cpp
+++ InspectionCore/contrib/MJPEGWriter/main.cpp
@@ -16,7 +16,44 @@ main()
     test.write(frame);
     frame.release();
     test.start();
-    while(cap.isOpened()){cap >> frame; test.write(frame); frame.release();}
+
+    Mat blank1 = Mat::zeros(Size(16,8),CV_8UC1);
+
+
+    int frameCode=0;
+    while(cap.isOpened()){
+      cap >> frame; 
+
+      frameCode++;
+      // if(frameCode%10!=0)continue;
+      // if(rand()%10!=0)continue;
+      int tmpT=frameCode;
+      for(int x=0;x<16;x++)
+      {
+          // get pixel
+          Vec3b & color = frame.at<Vec3b>(0,x);
+
+
+          color[0] = 255*(tmpT&1);
+          tmpT>>=1;
+          color[1] = 255*(tmpT&1);
+          tmpT>>=1;
+          color[2] = 255*(tmpT&1);
+          tmpT>>=1;
+
+      }
+
+      cv::putText(frame, //target image
+            "Hello, OpenCV!", //text
+            cv::Point(10, frame.rows / 2), //top-left position
+            cv::FONT_HERSHEY_DUPLEX,
+            1.0,
+            CV_RGB(118, 185, 0), //font color
+            2);
+      test.write(frame); frame.release();
+      // test.write(blank1);
+      
+    }
     test.stop();
     exit(0);
 
