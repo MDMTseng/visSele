3e8f3af8f19dc1a13415b07fc7e92d4b699ea2d3
MDMTseng <chiapintseng@gmail.com>
2021-04-25 22:35:18 +0800

    Core: try to speed up windows print speed (the new line is \r\n), also, set print strategy as IOFBF(means print when buffer assigned is full)

--- InspectionCore/logctrl/include/logctrl.h
+++ InspectionCore/logctrl/include/logctrl.h
@@ -22,6 +22,11 @@ char* _SubString_Align(const char* str,int Count);
 // #define LOGD(fmt,...) logd("%s:d " fmt "\n",__func__ VA_ARGS(__VA_ARGS__))
 // #define LOGI(fmt,...) logi("%s:i " fmt "\n",__func__ VA_ARGS(__VA_ARGS__))
 // #define LOGE(fmt,...) loge("%s:e " fmt "\n",__func__ VA_ARGS(__VA_ARGS__))
+#ifdef __WIN32__
+#define __BR__ "\r\n"
+#else
+#define __BR__ "\n"
+#endif
 #define LOG_FILENAME_L 10
 #define LOG_HAS_LINENUMBER 
 #ifdef LOG_HAS_LINENUMBER
@@ -30,15 +35,15 @@ char* _SubString_Align(const char* str,int Count);
 
 #define LOGV(fmt,...) //logv("%s%04d %s:v " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
 #define LOGD(fmt,...) //logd("%s%04d %s:d " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
-#define LOGI(fmt,...) logi("%s%04d %s:i " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
-#define LOGE(fmt,...) loge("%s%04d %s:e " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
+#define LOGI(fmt,...) logi("%s%04d %s:i " fmt __BR__,_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
+#define LOGE(fmt,...) loge("%s%04d %s:e " fmt __BR__,_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
 // #define LOGE(fmt,...) loge("%f:%s%04d %s:e " fmt "\n",C_TIME,_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
 #define LOGS(buf,fmt,...) sprintf(buf,"%f:%s%04d %s:$ " fmt "\n",C_TIME,_SubString_Align(__FILENAME__,LOG_FILENAME_L),__LINE__,__func__ VA_ARGS(__VA_ARGS__))
 #else
-#define LOGV(fmt,...) logv("%s %s:v " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__func__ VA_ARGS(__VA_ARGS__))
-#define LOGD(fmt,...) logd("%s %s:d " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__func__ VA_ARGS(__VA_ARGS__))
-#define LOGI(fmt,...) logi("%s %s:i " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__func__ VA_ARGS(__VA_ARGS__))
-#define LOGE(fmt,...) loge("%s %s:e " fmt "\n",_SubString_Align(__FILENAME__,LOG_FILENAME_L),__func__ VA_ARGS(__VA_ARGS__))
+#define LOGV(fmt,...) logv("%s %s:v " fmt __BR__,_SubString_Align(__FILENAME__,LOG_FILENAME_L),__func__ VA_ARGS(__VA_ARGS__))
+#define LOGD(fmt,...) logd("%s %s:d " fmt __BR__,_SubString_Align(__FILENAME__,LOG_FILENAME_L),__func__ VA_ARGS(__VA_ARGS__))
+#define LOGI(fmt,...) logi("%s %s:i " fmt __BR__,_SubString_Align(__FILENAME__,LOG_FILENAME_L),__func__ VA_ARGS(__VA_ARGS__))
+#define LOGE(fmt,...) loge("%s %s:e " fmt __BR__,_SubString_Align(__FILENAME__,LOG_FILENAME_L),__func__ VA_ARGS(__VA_ARGS__))
 
 #endif
 
