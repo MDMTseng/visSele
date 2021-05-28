45ce5f1687de6416869a9cb8b2bba26a95b22e71
Chia-Pin Tseng <chiapintseng@gmail.com>
2021-05-28 21:59:52 +0800

    Core0_1: CameraLayer_Aravis

--- InspectionCore/CameraLayer/CameraLayer_Aravis.cpp
+++ InspectionCore/CameraLayer/CameraLayer_Aravis.cpp
@@ -1,72 +1,110 @@
 
 #include "CameraLayer_Aravis.hpp"
 #include "logctrl.h"
+#include <arv.h>
 
-
-CameraLayer_Aravis::CameraLayer_Aravis(CameraLayer_Callback cb,void* context):CameraLayer(cb,context)
+CameraLayer_Aravis::CameraLayer_Aravis(CameraLayer_Callback cb, void *context) : CameraLayer(cb, context), arv_option_entries{
+                                                                                                               {"name", 'n', 0, G_OPTION_ARG_STRING,
+                                                                                                                &arv_option_camera_name, "Camera name", NULL},
+                                                                                                               {"snapshot", 's', 0, G_OPTION_ARG_NONE,
+                                                                                                                &arv_option_snaphot, "Snapshot", NULL},
+                                                                                                               {"frequency", 'f', 0, G_OPTION_ARG_DOUBLE,
+                                                                                                                &arv_option_frequency, "Acquisition frequency", NULL},
+                                                                                                               {"trigger", 't', 0, G_OPTION_ARG_STRING,
+                                                                                                                &arv_option_trigger, "External trigger", NULL},
+                                                                                                               {"software-trigger", 'o', 0, G_OPTION_ARG_DOUBLE,
+                                                                                                                &arv_option_software_trigger, "Emit software trigger", NULL},
+                                                                                                               {"width", 'w', 0, G_OPTION_ARG_INT,
+                                                                                                                &arv_option_width, "Width", NULL},
+                                                                                                               {"height", 'h', 0, G_OPTION_ARG_INT,
+                                                                                                                &arv_option_height, "Height", NULL},
+                                                                                                               {"h-binning", '\0', 0, G_OPTION_ARG_INT,
+                                                                                                                &arv_option_horizontal_binning, "Horizontal binning", NULL},
+                                                                                                               {"v-binning", '\0', 0, G_OPTION_ARG_INT,
+                                                                                                                &arv_option_vertical_binning, "Vertical binning", NULL},
+                                                                                                               {"exposure", 'e', 0, G_OPTION_ARG_DOUBLE,
+                                                                                                                &arv_option_exposure_time_us, "Exposure time (µs)", NULL},
+                                                                                                               {"gain", 'g', 0, G_OPTION_ARG_INT,
+                                                                                                                &arv_option_gain, "Gain (dB)", NULL},
+                                                                                                               {"auto", 'a', 0, G_OPTION_ARG_NONE,
+                                                                                                                &arv_option_auto_socket_buffer, "Auto socket buffer size", NULL},
+                                                                                                               {"packet-size-adjustment", 'j', 0, G_OPTION_ARG_STRING,
+                                                                                                                &arv_option_packet_size_adjustment, "Packet size adjustment",
+                                                                                                                "{never|always|once|on-failure|on-failure-once}"},
+                                                                                                               {"no-packet-resend", 'r', 0, G_OPTION_ARG_NONE,
+                                                                                                                &arv_option_no_packet_resend, "No packet resend", NULL},
+                                                                                                               {"packet-request-ratio", 'q', 0, G_OPTION_ARG_DOUBLE,
+                                                                                                                &arv_option_packet_request_ratio, "Packet resend request limit as a frame packet number ratio [0..2.0]", NULL},
+                                                                                                               {"packet-timeout", 'p', 0, G_OPTION_ARG_INT,
+                                                                                                                &arv_option_packet_timeout, "Packet timeout (ms)", NULL},
+                                                                                                               {"frame-retention", 'm', 0, G_OPTION_ARG_INT,
+                                                                                                                &arv_option_frame_retention, "Frame retention (ms)", NULL},
+                                                                                                               {"gv-stream-channel", 'c', 0, G_OPTION_ARG_INT,
+                                                                                                                &arv_option_gv_stream_channel, "GigEVision stream channel id", NULL},
+                                                                                                               {"gv-packet-delay", 'y', 0, G_OPTION_ARG_INT,
+                                                                                                                &arv_option_gv_packet_delay, "GigEVision packet delay (ns)", NULL},
+                                                                                                               {"gv-packet-size", 'i', 0, G_OPTION_ARG_INT,
+                                                                                                                &arv_option_gv_packet_size, "GigEVision packet size (bytes)", NULL},
+                                                                                                               {"chunks", 'u', 0, G_OPTION_ARG_STRING,
+                                                                                                                &arv_option_chunks, "Chunks", NULL},
+                                                                                                               {"realtime", '\0', 0, G_OPTION_ARG_NONE,
+                                                                                                                &arv_option_realtime, "Make stream thread realtime", NULL},
+                                                                                                               {"high-priority", '\0', 0, G_OPTION_ARG_NONE,
+                                                                                                                &arv_option_high_priority, "Make stream thread high priority", NULL},
+                                                                                                               {"no-packet-socket", '\0', 0, G_OPTION_ARG_NONE,
+                                                                                                                &arv_option_no_packet_socket, "Disable use of packet socket", NULL},
+                                                                                                               {"debug", 'd', 0, G_OPTION_ARG_STRING,
+                                                                                                                &arv_option_debug_domains, "Debug domains", NULL},
+                                                                                                               {"bandwidth-limit", 'b', 0, G_OPTION_ARG_INT,
+                                                                                                                &arv_option_bandwidth_limit, "Desired USB3 Vision device bandwidth limit", NULL},
+                                                                                                               {NULL}}
 {
+
+
+
+
 }
 
 CameraLayer_Aravis::~CameraLayer_Aravis()
 {
 }
-
-
-
-CameraLayer::status CameraLayer_Aravis::EnumerateDevice(tSdkCameraDevInfo * pCameraList,INT * piNums)
+CameraLayer::status CameraLayer_Aravis::SetMirror(int Dir, int en)
 {
-    return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
-void CameraLayer_Aravis::sGIGEMV_CB(CameraHandle hCamera, BYTE *frameBuffer, tSdkFrameHead* frameInfo,PVOID pContext)
+CameraLayer::status CameraLayer_Aravis::SetROIMirror(int Dir, int en)
 {
-}
-
-void CameraLayer_Aravis::GIGEMV_CB(CameraHandle hCamera, BYTE *frameBuffer, tSdkFrameHead* frameInfo,PVOID pContext)
-{
-}
-
-CameraLayer::status CameraLayer_Aravis::InitCamera(tSdkCameraDevInfo *devInfo)
-{
-
-}
-CameraLayer::status CameraLayer_Aravis::SetMirror(int Dir,int en)
-{
-  return CameraLayer::ACK;
-}
-CameraLayer::status CameraLayer_Aravis::SetROIMirror(int Dir,int en)
-{
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
 
 CameraLayer::status CameraLayer_Aravis::L_TriggerMode(int type)
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
 
-CameraLayer::status CameraLayer_Aravis::SetROI(float x, float y, float w, float h,int zw,int zh)
+CameraLayer::status CameraLayer_Aravis::SetROI(float x, float y, float w, float h, int zw, int zh)
 {
 
   return CameraLayer::NAK;
 }
-CameraLayer::status  CameraLayer_Aravis::GetROI(float *x, float *y, float *w, float *h,int*zw,int *zh)
+CameraLayer::status CameraLayer_Aravis::GetROI(float *x, float *y, float *w, float *h, int *zw, int *zh)
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
 
 CameraLayer::status CameraLayer_Aravis::TriggerMode(int type)
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
-
 
 CameraLayer::status CameraLayer_Aravis::TriggerCount(int count)
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
 
 CameraLayer::status CameraLayer_Aravis::Trigger()
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
 
 CameraLayer::status CameraLayer_Aravis::RUN()
@@ -74,69 +112,59 @@ CameraLayer::status CameraLayer_Aravis::RUN()
   return CameraLayer::NAK;
 }
 
-void CameraLayer_Aravis::ContTriggerThread( )
+void CameraLayer_Aravis::ContTriggerThread()
 {
 }
 
-
-
-void CameraLayer_Aravis::ContTriggerThreadTermination( )
+void CameraLayer_Aravis::ContTriggerThreadTermination()
 {
 }
 
-CameraLayer::status CameraLayer_Aravis::SetResolution(int width,int height)
+CameraLayer::status CameraLayer_Aravis::SetResolution(int width, int height)
 {
   return CameraLayer::NAK;
 }
 CameraLayer::status CameraLayer_Aravis::SetAnalogGain(int gain)
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
-
-
 
 CameraLayer::status CameraLayer_Aravis::L_SetFrameRateMode(int mode)
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
-
-
 
 CameraLayer::status CameraLayer_Aravis::SetOnceWB()
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
-
 
 CameraLayer::status CameraLayer_Aravis::SetFrameRateMode(int mode)
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
-
-
-
 
 CameraLayer::status CameraLayer_Aravis::SnapFrame()
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
 
-CameraLayer::status CameraLayer_Aravis::GetAnalogGain(int *ret_min,int *ret_max)
+CameraLayer::status CameraLayer_Aravis::GetAnalogGain(int *ret_min, int *ret_max)
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
 
 CameraLayer::status CameraLayer_Aravis::SetExposureTime(double time_us)
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
 
 CameraLayer::status CameraLayer_Aravis::GetExposureTime(double *ret_time_ms)
 {
-  return CameraLayer::ACK;
+  return CameraLayer::NAK;
 }
 
-acvImage* CameraLayer_Aravis::GetFrame()
+acvImage *CameraLayer_Aravis::GetFrame()
 {
   return &img;
 }
--- InspectionCore/CameraLayer/include/CameraLayer_Aravis.hpp
+++ InspectionCore/CameraLayer/include/CameraLayer_Aravis.hpp
@@ -9,52 +9,78 @@
 #include <mutex>
 #include <queue>
 #include <thread>
+#include <arv.h>
+class CameraLayer_Aravis : public CameraLayer
+{
 
-#include "CameraApi.h"
-class CameraLayer_Aravis : public CameraLayer{
+protected:
+  ArvCamera *camera;
+  ArvStream *stream;
 
-    protected:
-    static void sGIGEMV_CB(CameraHandle hCamera, BYTE *frameBuffer, tSdkFrameHead* frameInfo,PVOID pContext);
-    void GIGEMV_CB(CameraHandle hCamera, BYTE *frameBuffer, tSdkFrameHead* frameInfo,PVOID pContext);
+  char *arv_option_camera_name = NULL;
+  char *arv_option_debug_domains = NULL;
+  gboolean arv_option_snaphot = FALSE;
+  char *arv_option_trigger = NULL;
+  double arv_option_software_trigger = -1;
+  double arv_option_frequency = -1.0;
+  int arv_option_width = -1;
+  int arv_option_height = -1;
+  int arv_option_horizontal_binning = -1;
+  int arv_option_vertical_binning = -1;
+  double arv_option_exposure_time_us = -1;
+  int arv_option_gain = -1;
+  gboolean arv_option_auto_socket_buffer = FALSE;
+  char *arv_option_packet_size_adjustment = NULL;
+  gboolean arv_option_no_packet_resend = FALSE;
+  double arv_option_packet_request_ratio = -1.0;
+  unsigned int arv_option_packet_timeout = 20;
+  unsigned int arv_option_frame_retention = 100;
+  int arv_option_gv_stream_channel = -1;
+  int arv_option_gv_packet_delay = -1;
+  int arv_option_gv_packet_size = -1;
+  gboolean arv_option_realtime = FALSE;
+  gboolean arv_option_high_priority = FALSE;
+  gboolean arv_option_no_packet_socket = FALSE;
+  char *arv_option_chunks = NULL;
+  unsigned int arv_option_bandwidth_limit = -1;
 
-    public:
-    
-    CameraLayer_Aravis(CameraLayer_Callback cb,void* context);
-    CameraLayer::status EnumerateDevice(tSdkCameraDevInfo * pCameraList,INT * piNums);
-    CameraLayer::status InitCamera(tSdkCameraDevInfo *devInfo);
-
-    CameraLayer::status TriggerMode(int type);
-
-    CameraLayer::status TriggerCount(int count);
-    CameraLayer::status Trigger();
-    CameraLayer::status RUN();
-    CameraLayer::status SetResolution(int width,int height);
-    CameraLayer::status SetAnalogGain(int gain);
-    CameraLayer::status SetROI(float x, float y, float w, float h,int zw,int zh);
-
-    CameraLayer::status GetROI(float *x, float *y, float *w, float *h,int*zw,int *zh);
-    CameraLayer::status SnapFrame();
-
-    CameraLayer::status SetOnceWB();
-    
-    CameraLayer::status SetMirror(int Dir,int en);
-    CameraLayer::status SetROIMirror(int Dir,int en);
-
-    CameraLayer::status SetFrameRateMode(int mode);
-    CameraLayer::status GetAnalogGain(int *ret_min,int *ret_max);
-    CameraLayer::status SetExposureTime(double time_us);
-    CameraLayer::status GetExposureTime(double *ret_time_us);
-    void ContTriggerThread();
-    void ContTriggerThreadTermination();
-    acvImage* GetFrame();
+  const GOptionEntry arv_option_entries[];
 
 
-    
-    CameraLayer::status L_TriggerMode(int type);
-    CameraLayer::status L_SetFrameRateMode(int type);
+public:
+  CameraLayer_Aravis(CameraLayer_Callback cb, void *context);
+  // CameraLayer::status EnumerateDevice(tSdkCameraDevInfo *pCameraList, INT *piNums);
+  // CameraLayer::status InitCamera(tSdkCameraDevInfo *devInfo);
 
-    ~CameraLayer_Aravis();
+  CameraLayer::status TriggerMode(int type);
+
+  CameraLayer::status TriggerCount(int count);
+  CameraLayer::status Trigger();
+  CameraLayer::status RUN();
+  CameraLayer::status SetResolution(int width, int height);
+  CameraLayer::status SetAnalogGain(int gain);
+  CameraLayer::status SetROI(float x, float y, float w, float h, int zw, int zh);
+
+  CameraLayer::status GetROI(float *x, float *y, float *w, float *h, int *zw, int *zh);
+  CameraLayer::status SnapFrame();
+
+  CameraLayer::status SetOnceWB();
+
+  CameraLayer::status SetMirror(int Dir, int en);
+  CameraLayer::status SetROIMirror(int Dir, int en);
+
+  CameraLayer::status SetFrameRateMode(int mode);
+  CameraLayer::status GetAnalogGain(int *ret_min, int *ret_max);
+  CameraLayer::status SetExposureTime(double time_us);
+  CameraLayer::status GetExposureTime(double *ret_time_us);
+  void ContTriggerThread();
+  void ContTriggerThreadTermination();
+  acvImage *GetFrame();
+
+  CameraLayer::status L_TriggerMode(int type);
+  CameraLayer::status L_SetFrameRateMode(int type);
+
+  ~CameraLayer_Aravis();
 };
-
 
 #endif
