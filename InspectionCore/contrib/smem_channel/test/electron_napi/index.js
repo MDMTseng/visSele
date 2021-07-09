ad64fd5b84e6a3ad0876c4d93100b7fad7e5be58
Chia-Pin Tseng <chiapintseng@gmail.com>
2021-07-09 22:18:06 +0800

    Core: smem_channel test update

--- InspectionCore/contrib/smem_channel/test/electron_napi/index.js
+++ InspectionCore/contrib/smem_channel/test/electron_napi/index.js
@@ -10,9 +10,28 @@ emitter.on('start', () => {
 })
 
 let preTime=Date.now();
+let buffer_RECV=undefined;
+let buffer_SEND=undefined;
+
+
+emitter.on('buffer_RECV', (evt) => {
+  buffer_RECV=evt;
+})
+
+emitter.on('buffer_SEND', (evt) => {
+  buffer_SEND=evt;
+})
+
+
 emitter.on('data', (evt) => {
     let curTime=Date.now();
-    console.log(curTime-preTime,evt);
+
+    console.log(curTime-preTime,buffer_RECV);
+    // let i=0;
+    // for(i=0;i<1000;i++)
+    // {
+    //   console.log(buffer_RECV[i]);
+    // }
     preTime=curTime;
 })
 
--- InspectionCore/contrib/smem_channel/test/electron_napi/src/emit-from-cpp.cc
+++ InspectionCore/contrib/smem_channel/test/electron_napi/src/emit-from-cpp.cc
@@ -7,27 +7,42 @@
 
 smem_channel ch_server_recv("RECVXXX",40000,true);
 
+void finalizer__(Napi::Env /*env*/, uint8_t* data)
+{
+
+    printf("finalizer__>>>\n");
+}
+
+// uint8_t tmpArr_RECV[10000]={0};
+uint8_t tmpArr_SEND[10000]={0};
 
 Napi::Value CallEmit(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
   Napi::Function emit = info[0].As<Napi::Function>();
   emit.Call({Napi::String::New(env, "start")});
-  for (int i = 0; i < 3; i++) {
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    emit.Call(
-        {Napi::String::New(env, "data"), Napi::String::New(env, "data ...")});
+
+
+  auto buf_RECV = Napi::Buffer<uint8_t>::New(env, (uint8_t *)ch_server_recv.getPtr(),ch_server_recv.size());
+  emit.Call({Napi::String::New(env, "buffer_RECV"), buf_RECV});
+
+  {
+    auto buf = Napi::Buffer<uint8_t>::New(env, tmpArr_SEND,sizeof(tmpArr_SEND),finalizer__);
+    emit.Call({Napi::String::New(env, "buffer_SEND"), buf});
+  }
+
+  for (int i = 0; i < 100; i++) {
+    // std::this_thread::sleep_for(std::chrono::seconds(3));
+    // emit.Call({Napi::String::New(env, "data"), Napi::String::New(env, "data ...")});
 
     // ch_server_recv.r_wait();
     // volatile uint8_t* data=(uint8_t*)ch_server_recv.getPtr();
 
 
-
-
-    // auto buf = Napi::Uint8Array::New(env, 100*10000);
-    // memcpy_s((void *)buf.Data(), 100, ptr, 100);
-    // emit.Call({Napi::String::New(env, "data"), buf});
-
-
+    uint8_t * data = buf_RECV.Data();
+    data[0]=i;
+    emit.Call({Napi::String::New(env, "data"), Napi::String::New(env, "data ...")});
+    data[0]=0;
+    printf(">%d>>\n",i);
 
 
 
