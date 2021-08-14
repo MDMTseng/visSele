3a3c608eaf572fa110fdb3584fe552efb41da89f
Chia-Pin Tseng <chiapintseng@gmail.com>
2021-08-14 15:04:19 +0800

    script Electron APP launcher update

--- scripts/launcher.js
+++ scripts/launcher.js
@@ -1,111 +1,154 @@
 
-const {spawn, execFile,exec } = require('child_process');
-var electron;
+const { spawn, execFile, exec } = require('child_process');
+const electron = require('electron')
+// var electron;
 const fs = require('fs');
 const path = require('path')
-exports.SimpleMessage = 'Hello world';
-let WebSocket;
-let WSTunnel;
+// const fetch = require('fetch')
 
-let mainWindow = undefined;
+let mongoose;
 
-function initWSTunnel()
-{
+exports.update={
+  // currentVersion:()=>{"0.2.8"},
+  getAppInfo:()=>({
+    "version":"0.2.8"
+  }),
+  getRemoteVersionList:(url="https://api.github.com/repos/MDMTseng/visSele/releases/latest")=>{
+    fetch(url)
+    .then(res =>res.json())
+    .then(res =>{ 
+        console.log(res)
+      
+      
+    })
+    .catch(err => console.log(err));
 
-  WSTunnel = new WebSocket.Server({ port: 9714 });
+  }
 
-  WSTunnel.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-      let p = JSON.parse(message);
-      let retData={
-        type:"NAK",
-        req_id:p.req_id,
-      }
-      if(p.type=="showOpenDialog")
+
+}
+
+
+
+// = function (url)
+// {
+//   // fs.mkdirSync(dir);
+//   console.log("<><><><");
+// }
+
+
+exports.setup = function (pak) {
+  console.log(pak);
+  let { WebSocket, mongoose, express } = pak;
+  // // This method will be called when Electron has finished
+  // // initialization and is ready to create browser windows.
+  // // Some APIs can only be used after this event occurs.
+  init(pak);
+
+  function LinkMongoDB(pak) {
+
+  }
+
+
+
+  function initWSTunnel() {
+
+    WSTunnel = new WebSocket.Server({ port: 9714 });
+
+    WSTunnel.on('connection', function connection(ws) {
+      ws.on('message', function incoming(message) {
+        let p = JSON.parse(message);
+        let retData = {
+          type: "NAK",
+          req_id: p.req_id,
+        }
+        if (p.type == "showOpenDialog") {
+          // console.log('cmd: %s', p);
+          electron.dialog.showOpenDialog(p.option).then(function (response) {
+            if (!response.canceled) {
+              retData.type = "ACK";
+              retData.filePaths = response.filePaths;
+            }
+            ws.send(JSON.stringify(retData));
+          }).catch((err) => {
+            ws.send(JSON.stringify(retData));
+          })
+        }
+        else {
+          // console.log('received: %s', message);
+          ws.send(JSON.stringify(retData));
+        }
+      });
+    });
+
+  }
+
+  function init(pak) {
+    let { APP_INFO_FILE_PATH,mainWindow } = pak;
+    // {electron,APP_INFO_FILE_PATH,WebSocket}
+
+    let APP_INFO={};
+    try {
+      rawdata = fs.readFileSync(APP_INFO_FILE_PATH);
+      APP_INFO=JSON.parse(rawdata);
+    } catch (err) {
+
+    }
+
+    initWSTunnel();
+
+    // mainWindow.loadFile(APP_INFO.APPContentPath + "/WebUI/index.html")
+    mainWindow.loadURL("http://0.0.0.0:8080/")
+    // mainWindow.webContents.openDevTools()
+
+
+    // Emitted when the window is closed.
+
+
+    var spawnX=undefined;
+    if (process.platform === "win32") {
+      // spawnX = spawn('./visSele.exe', [],{
+      //   cwd: "./res/Core/win32/",
+      //   env: process.env,
+      //   stdio: 'inherit'});   
+    }
+    else //if(process.platform === "darwin  ")
+    {
+      spawnX = spawn('./visSele', ["chdir=" + APP_INFO.APPDataDirPath], {
+        cwd: APP_INFO.APPContentPath + "/Core",
+        env: process.env,
+        stdio: 'inherit'
+      });
+    }
+
+    mainWindow.on('closed', () => {
+      if(spawnX!==undefined)
       {
-        console.log('cmd: %s', p);
-        electron.dialog.showOpenDialog(p.option).then(function (response) {
-          if (!response.canceled) {
-            retData.type="ACK";
-            retData.filePaths=response.filePaths;
-          }
-          ws.send(JSON.stringify(retData));
-        }).catch((err)=>{
-          ws.send(JSON.stringify(retData));
-        })
+        spawnX.kill('SIGINT');
+        spawnX=undefined;
       }
       else
       {
-        // console.log('received: %s', message);
-        ws.send(JSON.stringify(retData));
+
       }
+      electron.app.quit();
     });
-  });
-  
-}
 
-exports.setup=function(pak)
-{
-  let {APP_INFO_FILE_PATH}=pak;
-  // {electron,APP_INFO_FILE_PATH,WebSocket}
-  WebSocket=pak.WebSocket;
-  electron=pak.electron;
+    // spawnX.stdout.pipe(process.stdout);
+    // spawnX.stdout.setEncoding('utf8');
+    // spawnX.stdout.on('data', function (data) {
+    //   console.log('stdout: ' + data.toString());
+    // });
 
+    // spawnX.stderr.on('data', function (data) {
+    //   console.log('stderr: ' + data.toString());
+    // });
 
-  initWSTunnel();
-  let rawdata = fs.readFileSync(APP_INFO_FILE_PATH);
-  console.log("APP_INFO_FILE_PATH",APP_INFO_FILE_PATH);
-  let APP_INFO_INFO = JSON.parse(rawdata);
-  
-
-  mainWindow = new electron.BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true,
-      contextIsolation: false,
-    }
-  })
-  mainWindow.loadFile(APP_INFO_INFO.APPContentPath+"/WebUI/index.html")
-  
-  // mainWindow.webContents.openDevTools()
-
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    electron.app.quit();
-  });
-
-
-  var spawnX;
-  if(process.platform === "win32")
-  {
-    // spawnX = spawn('./visSele.exe', [],{
-    //   cwd: "./res/Core/win32/",
-    //   env: process.env,
-    //   stdio: 'inherit'});   
-  }
-  else //if(process.platform === "darwin  ")
-  {
-    spawnX = spawn('./visSele', ["chdir="+APP_INFO_INFO.APPDataDirPath],{
-      cwd: APP_INFO_INFO.APPContentPath+"/Core",
-      env: process.env,
-      stdio: 'inherit'});   
+    spawnX.on('exit', function (code) {
+      console.log('child process exited with code ', code);
+    });
   }
 
 
-  // spawnX.stdout.pipe(process.stdout);
-  // spawnX.stdout.setEncoding('utf8');
-  // spawnX.stdout.on('data', function (data) {
-  //   console.log('stdout: ' + data.toString());
-  // });
-
-  // spawnX.stderr.on('data', function (data) {
-  //   console.log('stderr: ' + data.toString());
-  // });
-
-  spawnX.on('exit', function (code) {
-    console.log('child process exited with code ' + code.toString());
-  });
 }
+
