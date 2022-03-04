518d2eecea472d72f70389fc3f93de576980bd72
Chia-Pin Tseng <chiapintseng@gmail.com>
2022-03-05 02:41:00 +0800

    Core: json_seg_parser rewrite

--- InspectionCore/common_lib/include/json_seg_parser.hpp
+++ InspectionCore/common_lib/include/json_seg_parser.hpp
@@ -6,13 +6,56 @@
 class json_seg_parser
 {
     protected:
-    char pch;
-    int jsonInStrState;
-    int jsonCurlyB_C;
-    int jsonSquareB_C;
+
+    
+    enum JSonState
+    {
+      NUL,
+      OBJ_KEY,
+      OBJ_SEP,
+      OBJ_VAL=3,
+      OBJ_END,
+      ARR_END,
+      STR,
+      VAL,
+      DAT=8,
+
+      ERR
+    };
+    JSonState levelStack[30];
+    int stackSize=0;
+
+
+    JSonState getStackHead(int idx=0);
+    bool pushStackHead(JSonState st);
+    bool popStackHead();
 
     public:
+    
+    enum RESULT
+    {
+      WAIT_NEXT=0,
+      WAIT_NEXT_SPACE,
+      OBJECT_START=2,
+      OBJECT_COMPLETE,
+      ARRAY_START=4,
+      ARRAY_COMPLETE,
+
+      KEY_START=6,
+      KEY_END,
+
+      VAL_START=8,
+      // VAL_END,
+
+      
+      STR_START=10,
+      STR_END,
+
+      ERROR=-1000,
+    };
+
+    const int ERROR_CODE=-10000;
     json_seg_parser();
     virtual void reset();
-    virtual int newChar(char ch);
+    virtual RESULT newChar(char ch);
 };
--- InspectionCore/common_lib/json_seg_parser.cpp
+++ InspectionCore/common_lib/json_seg_parser.cpp
@@ -1,5 +1,5 @@
 #include "json_seg_parser.hpp"
-
+#include "stdio.h"
 
 
 json_seg_parser::json_seg_parser(){
@@ -7,60 +7,262 @@ json_seg_parser::json_seg_parser(){
 }
 void json_seg_parser::reset()
 {
-    pch='\0';
-    jsonCurlyB_C=0;
-    jsonInStrState=0;
-    jsonSquareB_C=0;
+    stackSize=0;
 }
-int json_seg_parser::newChar(char ch){
-    int ret_val=0;
-    if(ch=='"' && pch!='\\')
+
+
+
+json_seg_parser::JSonState json_seg_parser::getStackHead(int idx)
+{
+  if(stackSize==0)return JSonState::NUL;
+  if(idx>=stackSize)return  JSonState::ERR;
+  return levelStack[stackSize-1-idx];
+}
+bool json_seg_parser::pushStackHead(JSonState st)
+{
+  if(stackSize==(sizeof(levelStack)/sizeof(levelStack[0])))return false;//full
+  levelStack[stackSize++]=st;
+  return true;
+}
+bool json_seg_parser::popStackHead()
+{
+  if(stackSize==0)return false;
+  stackSize--;
+  return true;
+}
+
+inline bool isSpace(char ch)
+{
+  return (ch==' ' || ch=='\n' ||ch=='\t' );
+}
+
+json_seg_parser::RESULT json_seg_parser::newChar(char ch){
+
+  
+  // printf("stack[%d]:",stackSize);
+  // for(int i=0;i<stackSize;i++)
+  // {
+  //   printf("%d,",levelStack[i]);
+  // }
+  // printf("\n");
+
+  switch(getStackHead())
+  {
+    case OBJ_KEY:
     {
-        jsonInStrState=!jsonInStrState;
+      if(ch=='"')
+      {
+        popStackHead();
+        pushStackHead(JSonState::STR);
+        return RESULT::KEY_START;
+      }
+      else if(isSpace(ch))
+      {
+        return RESULT::WAIT_NEXT;
+      }
+      else
+      {
+        return RESULT::ERROR;
+      }
+
+    }
+    case OBJ_SEP:
+    {
+      if(ch==':')
+      {
+        popStackHead();
+        return RESULT::WAIT_NEXT;//KEY_END;
+      }
+      else if(isSpace(ch))
+      {
+        return RESULT::WAIT_NEXT;
+      }
+      else
+      {
+        return RESULT::ERROR;
+      }
     }
 
-    if( (ch=='{' && pch=='}')||(ch=='[' && pch==']') )//End reset
-    {
-        reset();
-        ret_val=0;
-    }
+    case NUL:
+      if(ch=='{')
+      {
+        pushStackHead(JSonState::OBJ_END);
+        pushStackHead(JSonState::DAT);
+        pushStackHead(JSonState::OBJ_SEP);
+        pushStackHead(JSonState::OBJ_KEY);
+        return RESULT::OBJECT_START;
+      }
+      else if(ch=='[')
+      {
+        pushStackHead(JSonState::ARR_END);
+        pushStackHead(JSonState::DAT);
+        return RESULT::ARRAY_START;
+      }
+      else if(isSpace(ch) )
+      {
+        return RESULT::WAIT_NEXT;
+      }
+      else 
+      {
+        return RESULT::ERROR;
+      }
+    break;
+    case DAT:
+      if(isSpace(ch))
+      {
+        return RESULT::WAIT_NEXT;
+      }
+      else if(ch=='{')
+      {
+        popStackHead();
+        pushStackHead(JSonState::OBJ_END);
+        pushStackHead(JSonState::DAT);
+        pushStackHead(JSonState::OBJ_SEP);
+        pushStackHead(JSonState::OBJ_KEY);
+        return RESULT::OBJECT_START;
+      }
+      else if(ch=='[')
+      {
+        popStackHead();
+        pushStackHead(JSonState::ARR_END);
+        pushStackHead(JSonState::DAT);
+        return RESULT::ARRAY_START;
+      }
+      else if(ch=='"')
+      {
+        popStackHead();
+        pushStackHead(JSonState::STR);
+        return RESULT::STR_START;
+      }
+      else 
+      {
+        popStackHead();
+        pushStackHead(JSonState::VAL);
+        return RESULT::VAL_START;
+      }
 
-    if(!jsonInStrState)
-    {
-        if( (ch=='{' || ch=='[') && jsonCurlyB_C==0 && jsonSquareB_C==0 )
-        {
-            //start point
-            ret_val=1;
-        }
-        switch(ch)
-        {
-            case '{':
-                jsonCurlyB_C++;
-            break;
-            case '[':
-                jsonSquareB_C++;
-            break;
-            case '}':
-                jsonCurlyB_C--;
-            break;
-            case ']':
-                jsonSquareB_C--;
-            break;
-        }
+
+
+
+
+
+    break;
+    case OBJ_END:
+      if(ch=='}')
+      {
+        popStackHead();
+        return RESULT::OBJECT_COMPLETE;
+      }
+      else if(ch==',')
+      {
         
-        if( (ch=='}' || ch==']') && jsonCurlyB_C==0 && jsonSquareB_C==0 )
+        pushStackHead(JSonState::DAT);
+        pushStackHead(JSonState::OBJ_SEP);
+        pushStackHead(JSonState::OBJ_KEY);
+        return RESULT::WAIT_NEXT;
+      }
+      else if(isSpace(ch))
+      {
+        return RESULT::WAIT_NEXT;
+      }
+      else
+      {
+        return RESULT::ERROR;
+      }
+
+    break;
+    case ARR_END:
+      if(ch==']')
+      {
+        popStackHead();
+        return RESULT::ARRAY_COMPLETE;
+      }
+      else if(ch==',')
+      {
+        
+        pushStackHead(JSonState::DAT);
+        return RESULT::WAIT_NEXT;
+      }
+      else if(isSpace(ch))
+      {
+        return RESULT::WAIT_NEXT;
+      }
+      else
+      {
+        return RESULT::ERROR;
+      }
+    break;
+    case STR:
+      if(ch=='"')//end STR
+      {
+        popStackHead();
+        if(getStackHead()==JSonState::OBJ_SEP)
         {
-            //end point
-            ret_val=-1;
+          return RESULT::KEY_END;
         }
-        if(jsonCurlyB_C<0 || jsonSquareB_C<0 )//Error reset
+        return RESULT::STR_END;
+      }
+      return RESULT::WAIT_NEXT;
+    break;
+    case VAL:
+
+      if(ch=='}')
+      {
+        if(getStackHead(1)==JSonState::OBJ_END)
         {
-            reset();
-            ret_val=0;
+          popStackHead();
+          return newChar(ch);//instant run again
         }
-    }
-    pch=ch;
-    return ret_val;
+        else
+        {//shouldn't be here
+          return RESULT::ERROR;
+        }
+      }
+      else if(ch==']')
+      {
+        if(getStackHead(1)==JSonState::ARR_END)
+        {
+          popStackHead();
+          return newChar(ch);//instant run again
+        }
+        else
+        {//shouldn't be here
+          return RESULT::ERROR;
+        }
+      }
+      else if(ch==',')
+      {
+
+
+        if(getStackHead(1)==JSonState::ARR_END)//only if it's arr
+        {
+          popStackHead();
+          
+          pushStackHead(JSonState::DAT);//keep find next data
+          return newChar(ch);//instant run again
+        }
+        if(getStackHead(1)==JSonState::OBJ_END)//only if it's arr
+        {
+          popStackHead();
+          
+          return  newChar(ch);
+        }
+        else
+        {
+          return RESULT::ERROR;
+        }
+      }
+      else
+      {
+        return RESULT::WAIT_NEXT;
+      }
+
+
+
+    break;
+  }
+
+  return RESULT::ERROR;
 }
 
 
