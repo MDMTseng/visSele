5a30c2534581e9af548602c7e76eb44740a72065
Chia-Pin Tseng <chiapintseng@gmail.com>
2022-07-20 09:45:43 +0800

    MStepper: change board upload speed and strip out unused libs

--- Peripheral/MStepper/include/main.hpp
+++ Peripheral/MStepper/include/main.hpp
@@ -1,9 +1,9 @@
 #pragma once
 #include <Arduino.h>
 
-#include <WiFi.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
+// #include <WiFi.h>
+// #include <AsyncTCP.h>
+// #include <ESPAsyncWebServer.h>
 
 #include "UTIL.h"
 #include "RingBuf.hpp"
@@ -16,5 +16,5 @@ void setup_comm();
 void loop_comm();
 
 
-void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-             void *arg, uint8_t *data, size_t len);
+// void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+//              void *arg, uint8_t *data, size_t len);
