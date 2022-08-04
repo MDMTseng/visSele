0802228ad447d66de0dcd589700b6dfa77fb3982
Chia-Pin Tseng <chiapintseng@gmail.com>
2022-08-04 17:27:23 +0800

    Peripheral: ManualTapeReelInsp

---
+++ Peripheral/ManualTapeReelInsp/Data_Layer/Data_Layer_Protocol.cpp
@@ -0,0 +1,527 @@
+#include "Data_Layer_Protocol.hpp"
+#include "string.h"
+#include "stdio.h"
+
+
+Data_JsonRaw_Layer::Data_JsonRaw_Layer():Data_Layer_IF()// throw(std::runtime_error)
+{
+  packetID=0;
+  rawRECVL=0;
+  maxHeaderSize=base_headerLen+8;//for extend lrngth field
+}
+
+
+static uint32_t crc_table[16] = {
+    0x00000000, 0x1db71064, 0x3b6e20c8, 0x26d930ac,
+    0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
+    0xedb88320, 0xf00f9344, 0xd6d6a3e8, 0xcb61b38c,
+    0x9b64c2b0, 0x86d3d2d4, 0xa00ae278, 0xbdbdf21c
+};
+
+static uint32_t crc_update(uint32_t crc, uint8_t data)
+{
+    uint8_t tbl_idx;
+    tbl_idx = crc ^ (data >> (0 * 4));
+    crc = crc_table[tbl_idx & 0x0f] ^ (crc >> 4);
+    tbl_idx = crc ^ (data >> (1 * 4));
+    crc = crc_table[tbl_idx & 0x0f] ^ (crc >> 4);
+    return crc;
+}
+
+int Data_JsonRaw_Layer::ask_JsonRaw_version(){
+  char sendMsg[200];
+  sprintf(sendMsg,"{\"type\":\"ask_JsonRaw_version\",\"id\":100445,\"version\":\"%s\"}",VERSION);
+  return send_json_string(0,(uint8_t*)sendMsg,strlen(sendMsg),0);
+}
+int Data_JsonRaw_Layer::rsp_JsonRaw_version(){
+  char sendMsg[200];
+  sprintf(sendMsg,"{\"type\":\"rsp_JsonRaw_version\",\"id\":100446,\"version\":\"%s\"}",VERSION);
+  return send_json_string(0,(uint8_t*)sendMsg,strlen(sendMsg),0);
+}
+
+int Data_JsonRaw_Layer::send_RESET(){
+  const int RESET_PACKET_SIZE=(sizeof(RESET_PACKET)-1);
+  return send_json_string(0,(uint8_t*)RESET_PACKET,RESET_PACKET_SIZE,0);
+}
+
+
+int Data_JsonRaw_Layer::recv_jsonRaw_data(uint8_t *raw,int rawL,uint8_t opcode){
+  
+  if(opcode==1 )
+  {
+    
+    return 0;
+
+  }
+  return 0;
+
+
+}
+
+int Data_JsonRaw_Layer::send_json_string(int head_room,uint8_t *data,int len,int leg_room){
+  return send_data(head_room,data,len,leg_room);
+}
+
+int Data_JsonRaw_Layer::send_raw_data(int opcode,int head_room,uint8_t *data,int len,int leg_room){
+  int headRoomReq=base_headerLen;
+  if(len>65535)headRoomReq=base_headerLen+8;
+  else if(len>125)headRoomReq=base_headerLen+2;
+  else headRoomReq=base_headerLen;
+
+
+  if(head_room<headRoomReq)
+  {//TODO: head room not enough use tmp space to continue the process
+    return -1;
+  }
+  
+  int rawL=len;
+
+  {//move head
+    len+=headRoomReq;
+    data-=headRoomReq;
+    head_room-=headRoomReq;
+  }
+  
+  //Set header
+  data[0]=1;
+  data[1]=opcode;
+
+
+  for(int i=crcFieldIdx;i<lenFieldIdx;i++)//write crc after len
+  {
+    data[i]=0;
+  }
+
+
+  //Set header (length field range1<=125 < range2 <= 65535 < range3 <= max 64bit)
+
+  if(rawL>65535)
+  {
+      data[lenFieldIdx]=127;//mark the 64 bits length field
+      for(int i=0;i<8;i++)
+      {
+        data[lenFieldIdx+1+i]=(rawL>>(8*(7-i)) )&0xFF;
+      }
+  }
+  else if(rawL>125)
+  {
+      data[lenFieldIdx]=126;//mark the 16 bits length field
+      for(int i=0;i<2;i++)
+      {
+        data[lenFieldIdx+1+i]=(rawL>>(8*(1-i)) )&0xFF;
+      }
+  }
+  else
+  {
+      data[lenFieldIdx]=rawL;
+  }
+  //Set header END
+
+
+  uint32_t crc=0;
+  //calc crc
+  for(int i=0;i<len;i++)//includes header to calc crc
+  {
+    crc=crc_update(crc,data[i]);
+    // printf("crc>>%X<< %x\n",crc,data[i]);
+  }
+  for(int i=0;i<(lenFieldIdx-crcFieldIdx);i++)//write crc after len
+  {
+    data[crcFieldIdx+i]=(crc>>(8*(crcL-1-i)) )&0xFF;
+  }
+
+  return send_data(head_room,data,len,leg_room);
+}
+int Data_JsonRaw_Layer::send_string(int head_room,uint8_t *data,int len,int leg_room){
+  return send_raw_data(1, head_room,data, len, leg_room);
+}
+int Data_JsonRaw_Layer::send_binary(int head_room,uint8_t *data,int len,int leg_room){
+  return send_raw_data(2, head_room,data, len, leg_room);
+}
+
+int Data_JsonRaw_Layer::send_data(int head_room,uint8_t *data,int len,int leg_room){
+  if(downlayer_df==NULL)return -1;
+  return downlayer_df->send_data(head_room,data,len,leg_room);
+}
+int Data_JsonRaw_Layer::recv_data(uint8_t *data,int len, bool is_a_packet){
+
+
+
+  if(recvType!=RTYPE::INIT)
+  {
+    //is in packet receive state
+  }
+  int i;
+  for(i=0;i<len;i++)
+  {
+    char c=data[i];
+
+
+    if(recvType==RTYPE::INIT)
+    { 
+      if(c=='{'||c=='[')
+      {
+        recvType=RTYPE::JSON;
+        jsegp.reset();
+        jlevel=0;
+      }
+      else if(c==0x1)
+      {
+        recvType=RTYPE::JSONRAW;
+      }
+      // else if(c==' '||c=='\t'||c=='\n')
+      // {
+      //   continue;
+      // }
+      else
+      {
+      //  printf("E:%d  %c\n",c,c);
+        recvType=RTYPE::ERROR;
+        errorCode=ERROR_TYPE::INIT_CHAR_ERROR;
+        recv_ERROR(errorCode);
+      }
+      buffIdx=0;
+    }
+    
+
+    if(recvType!=RTYPE::INIT)
+    {
+      
+      dataBuff[buffIdx++]=c;
+
+      // if(buffIdx%20==1)
+      // {
+      //   printf("buffIdx  %d\n",buffIdx);
+      // }
+
+      if(buffIdx==sizeof(dataBuff))
+      {
+        recvType=RTYPE::ERROR;//full and enter error clean state
+        errorCode=ERROR_TYPE::RECV_BUFFER_FULL;
+        recv_ERROR(errorCode);
+      }
+      // if(recvType==RTYPE::JSON && buffIdx>100)recvType=RTYPE::ERROR;//long json is error~
+      switch(recvType)
+      {
+        case RTYPE::ERROR:{
+          //init
+          const int RESET_PACKET_SIZE=(sizeof(RESET_PACKET)-1);
+          if(buffIdx>=RESET_PACKET_SIZE)//buff filled with enough data
+          {
+            
+            // printf("buffIdx  %d >=RESET_PACKET_SIZE  %d \n",buffIdx,RESET_PACKET_SIZE);
+            // for(int j=0;j<buffIdx;j++)
+            // {
+            //   printf("%c",dataBuff[j]);
+            // }
+            // printf("\n");
+            
+
+
+
+            int matching_state=0;//0 for no matching, 1 for matching ok, 2 for matching ok but not complete(wait for upcoming data)
+            int dataShiftBackIdx=0;
+            for(int j=0;j<buffIdx;j++)
+            {
+
+              bool matchPass=true;
+              int k=0;
+              for(k=0;k<RESET_PACKET_SIZE && (j+k)<buffIdx  ;k++)
+              {
+                if(RESET_PACKET[k] != dataBuff[j+k])
+                {
+                  matchPass=false;
+                  break;
+                }
+                
+              }
+
+              if(matchPass)
+              {
+                //packet starts with j
+                //may need data shift
+                if(k<RESET_PACKET_SIZE)//incomplete matching
+                {
+                  matching_state=2;
+                  dataShiftBackIdx=j;
+                }
+                else
+                {
+                  //hit RESET
+                  matching_state=1;
+                  dataShiftBackIdx=j+RESET_PACKET_SIZE-1;//shift and skip reset packet
+                  recv_RESET();
+                  recvType=RTYPE::INIT;
+                }
+                break;
+              }
+              else
+              {
+                continue;
+              }
+
+
+
+            }
+
+            if(matching_state==0)
+            {
+              buffIdx=0;
+            }
+            else if(dataShiftBackIdx)
+            {
+              if(dataShiftBackIdx==buffIdx)//meaning reset idx
+              {
+                buffIdx=0;
+              }
+              else
+              {
+                for(int j=0;j+dataShiftBackIdx<buffIdx;j++)
+                {
+                  dataBuff[j]=dataBuff[j+dataShiftBackIdx];
+                }
+                buffIdx-=dataShiftBackIdx;
+              }
+            }
+
+          }
+
+          break;
+        }
+        
+        
+        
+        case RTYPE::JSON:{
+          json_seg_parser::RESULT stat=jsegp.newChar(c);
+          if(stat<=json_seg_parser::RESULT::ERROR)
+          {
+            // printf("jsegp.ERROR_CODE\n");
+            recvType=RTYPE::ERROR;
+            errorCode=ERROR_TYPE::JSON_FORMAT_ERROR;
+            recv_ERROR(errorCode);
+            break;
+          }
+
+          if(stat==json_seg_parser::RESULT::OBJECT_START||
+          stat==json_seg_parser::RESULT::ARRAY_START)
+          {
+            jlevel++;
+            
+            // printf("%c   lvl:%d \n",JSONStr[i],jlevel);
+          }
+          else if(
+            stat==json_seg_parser::RESULT::OBJECT_COMPLETE||
+            stat==json_seg_parser::RESULT::ARRAY_COMPLETE)
+          {
+            jlevel--;
+
+            if(jlevel==0)
+            {
+
+
+
+              dataBuff[buffIdx]='\0';
+              // if(uplayer_df!=NULL)
+              //   uplayer_df->recv_data(dataBuff,buffIdx);
+              recv_jsonRaw_data(dataBuff,buffIdx,1);//opcode 1 is for text
+              
+              recvType=RTYPE::INIT;
+              if(uplayer_df!=NULL)uplayer_df->recv_data(dataBuff,buffIdx,true);
+            }
+
+          }
+
+        break;
+        }
+        case RTYPE::JSONRAW:{
+          static int headerLen;
+          static uint8_t f1_r3_op4_code;
+          static uint64_t dataLen;
+          static uint32_t targ_crc;
+          static uint32_t calc_crc;
+
+          if(buffIdx==1)
+          {
+            headerLen=base_headerLen;
+            targ_crc=calc_crc=0;
+
+
+            
+            break;
+          }
+          if(buffIdx<headerLen)break;//accumulate for header
+          if(buffIdx==headerLen)
+          {
+            if(buffIdx==base_headerLen)//base info collected
+            {
+              f1_r3_op4_code=dataBuff[1];
+              targ_crc=0;
+              for(int i=crcFieldIdx;i<lenFieldIdx;i++)
+              {
+                targ_crc<<=8;
+                targ_crc|=dataBuff[i];
+                dataBuff[i]=0;//zero the field to calc real crc
+              }
+              dataLen=dataBuff[lenFieldIdx];
+              if(dataLen==126)
+              {
+                headerLen=base_headerLen+2;//update real header len with data size info 16bit
+              }
+              else if(dataLen==127)
+              {
+                headerLen=base_headerLen+8;//update real header len with data size info 64bit
+              }
+              break;
+            }
+
+            if(dataLen==126)
+            {//16bit
+              dataLen=(dataBuff[lenFieldIdx+1]<<8)+dataBuff[lenFieldIdx+2];
+              
+              // printf(">>dataLen:%d\n",dataLen);
+            }
+            if(dataLen==127)
+            {//64bit
+              dataLen=0;
+              for(int i=0;i<8;i++)
+              {
+                dataLen=(dataLen<<8)|dataBuff[i+lenFieldIdx+1];
+              }
+              // printf(">>dataLen:%d\n",dataLen);
+            }
+            if((headerLen+dataLen)>sizeof(dataBuff))
+            {
+              //TODO: ERROR, oversize
+              recvType=RTYPE::ERROR;
+              
+              errorCode=ERROR_TYPE::RAW_DATA_OVERSIZE;
+              recv_ERROR(errorCode);
+              break;
+            }
+          }
+          if(buffIdx==headerLen+1)
+          {
+            calc_crc=0;
+            for(int i=0;i<buffIdx;i++)//calc header crc
+            {
+              calc_crc=crc_update(calc_crc,dataBuff[i]);
+              // printf("calc_crc>>%X << %x\n",calc_crc,dataBuff[i]);
+            }
+          }
+          else
+          {
+            calc_crc=crc_update(calc_crc,dataBuff[buffIdx-1]);
+          }
+
+          // printf("calc_crc>>%X << %x\n",calc_crc,dataBuff[buffIdx-1]);
+
+          // printf(">>%d:%d\n",buffIdx,headerLen+dataLen);
+          if(buffIdx==headerLen+dataLen)//the index reaches the headerLen+dataLen
+          {
+            if(calc_crc==targ_crc)
+            {
+              uint8_t opcode = f1_r3_op4_code&0xF;
+              if(opcode ==1)
+              {//string ends with zero
+                dataBuff[buffIdx]='\0';
+              }
+              recv_jsonRaw_data(dataBuff+headerLen,dataLen,opcode);
+              
+              if(uplayer_df!=NULL)uplayer_df->recv_data(dataBuff+headerLen,dataLen,true);
+              recvType=RTYPE::INIT;
+            }
+            else
+            {
+              // printf("CRC miss match:tar_crc:%X  calc_crc:%X \n",targ_crc,calc_crc);
+              recvType=RTYPE::ERROR;
+              errorCode=ERROR_TYPE::RAW_CRC_ERROR;
+              recv_ERROR(errorCode);
+              break;
+            }
+          }
+
+
+
+        break;
+        }
+      }
+    }
+
+    // printf("stid>%d\n",jsegpSum);
+  }
+  
+
+  if(recvType!=RTYPE::INIT)
+  {
+    //is in packet receive state
+  }
+  return 0;
+
+
+}
+
+
+
+
+
+
+
+// int Data_JsonRaw_Layer::sendString(Data_JsonRaw_Layer *JRL,uint8_t* buf, int bufL, bool directStringFormat, cJSON* json)
+// {
+
+//   if (JRL==NULL)
+//   {
+//     return -1;
+//   }
+//   int buff_head_room=JRL->max_head_room_size();
+//   int buffSize=bufL-buff_head_room;
+//   char *padded_buf=(char*)buf+buff_head_room;
+
+//   int ret= cJSON_PrintPreallocated(json, padded_buf, buffSize-JRL->max_leg_room_size(), false);
+
+//   if(ret == false)
+//   {
+//     return -1;
+//   }
+
+//   int contentSize=strlen(padded_buf);
+//   if(directStringFormat)
+//   {
+//     ret = perifCH->send_json_string(buff_head_room,(uint8_t*)padded_buf,contentSize,buffSize-contentSize);
+//   }
+//   else
+//   {
+//     ret = perifCH->send_string(buff_head_room,(uint8_t*)padded_buf,contentSize,buffSize-contentSize);
+//   }
+//   return ret;
+// }
+
+
+
+
+int Data_JsonRaw_Layer::send_printf(uint8_t* buf, int bufL, bool directStringFormat, const char *fmt, ...)
+{
+  int buff_head_room=max_head_room_size();
+  int buffSize=bufL-buff_head_room;
+  uint8_t *padded_buf=buf+buff_head_room;
+
+  va_list aptr;
+  int ret;
+  va_start(aptr, fmt);
+  ret = vsnprintf ((char*)padded_buf, buffSize-max_leg_room_size(), fmt, aptr);
+  va_end(aptr); 
+
+  if(ret<0)return ret;
+
+  int contentSize=ret;
+  
+  if(directStringFormat)
+  {
+    ret = send_json_string(buff_head_room,padded_buf,contentSize,buffSize-contentSize);
+  }
+  else
+  {
+    ret = send_string(buff_head_room,padded_buf,contentSize,buffSize-contentSize);
+  }
+  return ret;
+}
---
+++ Peripheral/ManualTapeReelInsp/main.cpp
@@ -0,0 +1,495 @@
+
+#include "main.hpp"
+
+#include <ESP32Encoder.h>
+
+#include <Data_Layer_Protocol.hpp>
+#include <ArduinoJson.h>
+#define __PRT_D_(fmt,...) //djrl.dbg_printf("%04d %.*s:d " fmt,__LINE__,PRT_FUNC_LEN,__func__ , ##__VA_ARGS__)
+// #define __PRT_I_(...) Serial.printf("I:" __VA_ARGS__)
+#define __PRT_I_(fmt,...) djrl.dbg_printf("%04d %.*s:i " fmt,__LINE__,PRT_FUNC_LEN,__func__ , ##__VA_ARGS__)
+
+const int O_LEDPin = 2;
+//O => 26 25 33 32
+//I => 17 18 19 23
+
+const int O_CameraPin = 33;
+const int O_BackLight = 32;
+const int O_EM_STOP = 25;
+const int O_TBD = 26;
+
+
+const int I_EncAPin = 17;
+const int I_EncBPin = 18;
+
+
+bool O_CameraPin_ON=true;
+bool O_BackLight_ON=true;
+bool O_EM_STOP_ON=true;
+bool I_Enc_Inv=true;
+
+// twoGateSense tGS;
+int g_cam_trig_delay=85;//from object detected how long to trigger camera
+int g_flash_trig_delay=80;//from object detected how long to flash
+int g_flash_time=100;//flash how long
+
+
+void genMachineSetup(JsonDocument &jdoc)
+{
+
+  jdoc["cam_trig_delay"]=g_cam_trig_delay;
+  jdoc["flash_trig_delay"]=g_flash_trig_delay;
+  jdoc["flash_time"]=g_flash_time;
+  // jdoc["pulse_sep_min"]=g_pulse_sep_min;
+  // jdoc["pulse_width_min"]=g_pulse_width_min;
+  // jdoc["pulse_width_max"]=g_pulse_width_max;
+  // jdoc["pulse_debounce_high"]=g_pulse_debounce_high;
+  // jdoc["pulse_debounce_low"]=g_pulse_debounce_low;
+
+  jdoc["O_CameraPin_ON"]=O_CameraPin_ON;
+  jdoc["O_BackLight_ON"]=O_BackLight_ON;
+  jdoc["O_EM_STOP_ON"]=O_EM_STOP_ON;
+  jdoc["I_Enc_Inv"]=I_Enc_Inv;
+
+}
+
+#define JSON_SETIF_ABLE(tarVar,jsonObj,key) \
+  {if(jsonObj[key].is<typeof(tarVar)>()  ) tarVar=jsonObj[key];}
+
+void setMachineSetup(JsonDocument &jdoc)
+{
+  JSON_SETIF_ABLE(g_cam_trig_delay,jdoc,"cam_trig_delay");
+  JSON_SETIF_ABLE(g_flash_trig_delay,jdoc,"flash_trig_delay");
+  JSON_SETIF_ABLE(g_flash_time,jdoc,"flash_time");
+  // JSON_SETIF_ABLE(g_pulse_sep_min,jdoc,"pulse_sep_min");
+  // JSON_SETIF_ABLE(g_pulse_width_min,jdoc,"pulse_width_min");
+  // JSON_SETIF_ABLE(g_pulse_width_max,jdoc,"pulse_width_max");
+  // JSON_SETIF_ABLE(g_pulse_debounce_high,jdoc,"pulse_debounce_high");
+  // JSON_SETIF_ABLE(g_pulse_debounce_low,jdoc,"pulse_debounce_low");
+  JSON_SETIF_ABLE(g_cam_trig_delay,jdoc,"cam_trig_delay");
+
+
+  JSON_SETIF_ABLE(O_CameraPin_ON,jdoc,"O_CameraPin_ON");
+  JSON_SETIF_ABLE(O_BackLight_ON,jdoc,"O_BackLight_ON");
+  JSON_SETIF_ABLE(O_EM_STOP_ON,jdoc,"O_EM_STOP_ON");
+  JSON_SETIF_ABLE(I_Enc_Inv,jdoc,"I_Enc_Inv");
+}
+
+static void enc_cb(void* arg);
+ESP32Encoder encoder(true, enc_cb);
+int32_t count=0;
+static bool leds = false;
+
+
+
+
+static IRAM_ATTR void enc_cb(void* arg) {
+  //Serial.printf("enc cb: count: %d\n", enc->getCount());
+  count=encoder.getCount();
+  // digitalWrite(O_LEDPin, leds);
+  // leds=!leds;
+  int t=count%10;
+  switch(t)
+  {
+    case 0:
+      digitalWrite(O_LEDPin, 1);
+      digitalWrite(O_BackLight, 1);
+    break;
+    case 2:
+      digitalWrite(O_CameraPin, 1);
+      break;
+    case 3:
+    break;
+    case 4:
+      digitalWrite(O_BackLight, 0);
+      digitalWrite(O_CameraPin, 0);
+    break;
+    case 8:
+      digitalWrite(O_LEDPin, 0);
+    break;
+  }
+  
+
+}
+
+
+
+
+hw_timer_t *timer = NULL;
+#define S_ARR_LEN(arr) (sizeof(arr) / sizeof(arr[0]))
+
+
+#define TRACKING_LEN 10
+
+
+
+
+
+
+void IRAM_ATTR onTimer()
+{
+}
+void setup()
+{
+
+  pinMode(O_LEDPin, OUTPUT);
+  pinMode(O_CameraPin, OUTPUT);
+  pinMode(O_BackLight, OUTPUT);
+  pinMode(O_EM_STOP, OUTPUT);
+  
+  pinMode(I_EncAPin, INPUT_PULLUP);
+  pinMode(I_EncBPin, INPUT_PULLUP);
+  ESP32Encoder::useInternalWeakPullResistors=UP;
+  encoder.attachHalfQuad(I_EncAPin, I_EncBPin);
+  encoder.clearCount();
+  encoder.setFilter(1023);
+  // Serial.begin(921600);
+  Serial.begin(460800);
+
+
+  while(0)
+  {
+      
+    delay(200);
+    digitalWrite(O_CameraPin, 0);
+    digitalWrite(O_BackLight, 0);
+    
+    delay(200);
+    digitalWrite(O_CameraPin, 1);
+    digitalWrite(O_BackLight, 1);
+    __PRT_D_("OK_ g:%d\n",digitalRead(I_gate1Pin));
+  }
+  // // setup_comm();
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 100, true);
+  timerAlarmEnable(timer);
+
+  digitalWrite(O_BackLight, 1);
+  digitalWrite(O_CameraPin, 1);
+  delay(3000);
+  // digitalWrite(O_BackLight, 0);
+  digitalWrite(O_CameraPin, 0);
+  digitalWrite(O_BackLight, 0);
+}
+
+
+
+StaticJsonDocument <1024>doc;
+StaticJsonDocument  <1024>retdoc;
+
+class MData_uInsp:public Data_JsonRaw_Layer
+{
+  
+  public:
+  MData_uInsp():Data_JsonRaw_Layer()// throw(std::runtime_error)
+  {
+    sprintf(peerVERSION,"");
+  }
+  int recv_RESET()
+  {
+
+  } 
+  int recv_ERROR(ERROR_TYPE errorcode)
+  {
+
+  }
+
+  int recv_jsonRaw_data(uint8_t *raw,int rawL,uint8_t opcode){
+    
+    if(opcode==1 )
+    {
+      doc.clear();
+      retdoc.clear();
+      DeserializationError error = deserializeJson(doc, raw);
+      bool rspAck=false;
+      bool doRsp=false;
+
+      const char* type = doc["type"];
+      // const char* id = doc["id"];
+      if(strcmp(type,"RESET")==0)
+      {
+        return msg_printf("RESET_OK","");
+      }
+      else if(strcmp(type,"ask_JsonRaw_version")==0)
+      {
+        
+        const char* _version = doc["version"];
+        strcpy(peerVERSION,_version);
+        return this->rsp_JsonRaw_version();
+      }
+      else if(strcmp(type,"rsp_JsonRaw_version")==0)
+      {
+        const char* _version = doc["version"];
+        strcpy(peerVERSION,_version);
+        return 0;
+      }
+      else if(strcmp(type,"PING")==0)
+      {
+        retdoc["type"]="PONG"; 
+        doRsp=rspAck=true;
+      }
+      else if(strcmp(type,"get_setup")==0)
+      {
+
+        retdoc["type"]="get_setup";
+        retdoc["ver"]=VERSION;
+        genMachineSetup(retdoc);
+
+        
+        doRsp=rspAck=true;
+
+      }
+      else if(strcmp(type,"set_setup")==0)
+      {
+        retdoc["type"]="set_setup";
+        
+        setMachineSetup(doc);
+        doRsp=rspAck=true;
+
+      }
+      else if(strcmp(type,"PIN_CONF")==0)
+      {
+        
+        if(doc["pin"].is<int>())
+        {
+          int pinNo = doc["pin"];
+
+       
+          if(doc["mode"].is<int>())
+          {
+            int mode= doc["mode"];//0:input 1:output 2:INPUT_PULLUP 3:INPUT_PULLDOWN
+            switch(mode)
+            {
+              case 0:pinMode(pinNo, INPUT);break;
+              case 1:pinMode(pinNo, OUTPUT);break;
+              case 2:pinMode(pinNo, INPUT_PULLUP);break;
+              case 3:pinMode(pinNo, INPUT_PULLDOWN);break;
+            }
+          }
+          else if(doc["output"].is<int>())
+          {
+            int output= doc["output"];//0:input 1:output 2:INPUT_PULLUP 3:INPUT_PULLDOWN
+            switch(output)
+            {
+              case -2://analog
+              {
+                int value=analogRead(pinNo);
+                
+                retdoc["type"]="PIN_INFO";
+                retdoc["value"]=value;
+                doRsp=rspAck=true;
+
+                break;
+              }
+              case -1://digital
+              {
+                int value=digitalRead(pinNo);
+                
+                retdoc["type"]="PIN_INFO";
+                retdoc["value"]=value;
+                doRsp=rspAck=true;
+                break;
+              }
+              case 0:digitalWrite(pinNo, LOW);break;
+              case 1:digitalWrite(pinNo, HIGH);break;
+            }
+          }
+        }
+        else
+        {
+
+        }
+        
+
+
+      
+        // retdoc["type"]="DBG_PRT";
+        // // retdoc["msg"]=doc;
+        // retdoc["error"]=error.code();
+        // retdoc["id"]=doc["id"];
+        // uint8_t buff[300];
+        // int slen=serializeJson(retdoc, (char*)buff,sizeof(buff));
+        // send_json_string(0,buff,slen,0);
+      }
+      else if(strcmp(type,"BL_ON")==0)
+      {
+        digitalWrite(O_BackLight, O_BackLight_ON);
+        doRsp=rspAck=true;
+
+      }     
+      else if(strcmp(type,"BL_OFF")==0)
+      {
+        digitalWrite(O_BackLight, !O_BackLight_ON);
+        doRsp=rspAck=true;
+
+      }
+      else if(strcmp(type,"EM_STOP")==0)
+      {
+        int keep_ms=10;
+        if(doc["keep_ms"].is<int>())
+        {
+          keep_ms=doc["keep_ms"];
+        }
+        digitalWrite(O_EM_STOP, O_EM_STOP_ON);
+        delay(keep_ms);
+        digitalWrite(O_EM_STOP, !O_EM_STOP_ON);
+
+        doRsp=rspAck=true;
+
+      }
+      else if(strcmp(type,"EM_STOP_ON")==0)
+      {
+        digitalWrite(O_EM_STOP, O_EM_STOP_ON);
+        doRsp=rspAck=true;
+
+      }
+      else if(strcmp(type,"EM_STOP_OFF")==0)
+      {
+        digitalWrite(O_EM_STOP, !O_EM_STOP_ON);
+        doRsp=rspAck=true;
+
+      }
+      else if(strcmp(type,"Cam_Trigger")==0)
+      {
+        digitalWrite(O_CameraPin_ON, !O_CameraPin_ON);
+        delay(10);
+        digitalWrite(O_CameraPin_ON, O_CameraPin_ON);
+        delay(100);
+        digitalWrite(O_CameraPin_ON, !O_CameraPin_ON);
+
+
+        doRsp=rspAck=true;
+
+      }
+      else if(strcmp(type,"BYE")==0)
+      {
+        doRsp=rspAck=true;
+
+      }
+
+
+      if(doRsp)
+      {
+        retdoc["id"]=doc["id"];
+        retdoc["ack"]=rspAck;
+        
+        uint8_t buff[700];
+        int slen=serializeJson(retdoc, (char*)buff,sizeof(buff));
+        send_json_string(0,buff,slen,0);
+      }
+    }
+
+
+    return 0;
+
+
+  }
+  void connected(Data_Layer_IF* ch){}
+
+  int send_data(int head_room,uint8_t *data,int len,int leg_room){
+    Serial.write(data,len);
+    return 0;
+  }
+  void disconnected(Data_Layer_IF* ch){}
+  void DBGINFO()
+  {
+    
+  }
+
+  int close(){}
+
+  
+  char dbgBuff[500];
+  int dbg_printf(const char *fmt, ...)
+  {
+    char *str=dbgBuff;
+    int restL=sizeof(dbgBuff);
+    {//start head
+      int len=sprintf(str,"{\"dbg\":\"");
+      str+=len;
+      restL-=len;
+
+    }
+
+    {
+      va_list aptr;
+      int ret;
+      va_start(aptr, fmt);
+      ret = vsnprintf (str, restL-10, fmt, aptr);
+      va_end(aptr); 
+      str+=ret;
+      restL-=ret;
+
+
+    }
+    {//end
+      int len=sprintf(str,"\"}");
+      str+=len;
+      restL-=len;
+    }
+
+    return send_json_string(0,(uint8_t*)dbgBuff,str-dbgBuff,0);
+  }
+
+  int msg_printf(const char *type,const char *fmt, ...)
+  {
+    char *str=dbgBuff;
+    int restL=sizeof(dbgBuff);
+    {//start head
+      int len=sprintf(str,"{\"type\":\"%s\",\"data\":\"",type);
+      str+=len;
+      restL-=len;
+
+    }
+
+    {
+      va_list aptr;
+      int ret;
+      va_start(aptr, fmt);
+      ret = vsnprintf (str, restL-10, fmt, aptr);
+      va_end(aptr); 
+      str+=ret;
+      restL-=ret;
+
+
+    }
+    {//end
+      int len=sprintf(str,"\"}");
+      str+=len;
+      restL-=len;
+    }
+
+    return send_json_string(0,(uint8_t*)dbgBuff,str-dbgBuff,0);
+  }
+};
+
+
+
+
+
+MData_uInsp djrl;
+int32_t last_count=0;
+void loop()
+{
+  {
+    uint8_t recvBuf[50];
+    if (Serial.available() > 0) {
+      // read the incoming byte:
+      // char c=Serial.read();
+      // djrl.recv_data((uint8_t*)&c,1);
+      int recvLen = Serial.read(recvBuf,sizeof(recvBuf));
+      djrl.recv_data((uint8_t*)recvBuf,recvLen);
+
+      // djrl.dbg_printf(">>new data, len:%d",recvLen);
+      
+    }
+  }
+  // oGS.mainLoop();
+  // loop_comm();
+  if(last_count!=count)
+  {
+    djrl.dbg_printf("ENC:%06d \n",count);
+    last_count=count;
+  }
+}
